87cfbc5c99794d755c7d1944681b70727e491fa7
Hoyoung Jung <honux77@gmail.com>
2024-04-08 01:14:25 +0900

    스프링 부트 초기화

---
+++ src/test/java/codesquad/springcafe/SpringCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package codesquad.springcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
