da6ac98e302f6c3f93a99ac65d02855e283d6ca7
sangchu <didi1484@gmail.com>
2024-04-08 13:54:05 +0900

    feat: 회원 목록 조회 기능 구현
    - /users 로 접속시 회원 목록 조회 가능
    - 타임리프 이용하여 모델정보 동적으로 html에 적용

--- src/main/java/codesquad/springcafe/model/User.java
+++ src/main/java/codesquad/springcafe/model/User.java
@@ -5,6 +5,17 @@ public class User {
     private String password;
     private String name;
     private String email;
+    private Long sid;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
 
     public String getUserId() {
         return userId;
@@ -36,6 +47,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getSid() {
+        return sid;
+    }
+
+    public void setSid(Long sid) {
+        this.sid = sid;
     }
 
     @Override
