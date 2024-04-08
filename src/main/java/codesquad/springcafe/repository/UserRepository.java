32cac5b3fe99031d65e640a485164528745f40cb
yonghyeonpark <pyh4616@naver.com>
2024-04-08 11:43:58 +0900

    feat: UserRepository 클래스 구현
    - 동시성 고려
      - user 객체를 저장하는 users에 ConcurrentHashMap 사용
      - 순서를 나타내는 sequence에 AtomicLong 사용
    - user 객체를 저장하는 save 메서드 구현

---
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class UserRepository {
+
+    private final Map<Long, User> users;
+    private final AtomicLong sequence;
+
+    public UserRepository() {
+        this.users = new ConcurrentHashMap<>();
+        sequence = new AtomicLong();
+    }
+
+    public void save(User user) {
+        users.put(sequence.getAndIncrement(), user);
+    }
+}
