85d2b3821361a65a28ea22e291446fc742b103f9
yonghyeonpark <pyh4616@naver.com>
2024-04-08 20:13:18 +0900

    feat: "/users" url 처리
    - get
      - 유저 리스트 뷰를 반환

--- src/main/java/codesquad/springcafe/controller/UserController.java
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -25,4 +25,9 @@ public class UserController {
         userService.join(user);
         return "redirect:/users";
     }
+
+    @GetMapping("/users")
+    public String userList() {
+        return "/user/list";
+    }
 }
