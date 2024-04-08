ef786ce11ecddecc95d267cb37f9490b3e9d87e4
yonghyeonpark <pyh4616@naver.com>
2024-04-08 11:28:04 +0900

    feat: User 클래스 생성
    - id, password, name, email을 필드로 갖는 User 클래스 생성

---
+++ src/main/java/codesquad/springcafe/domain/User.java
@@ -0,0 +1,16 @@
+package codesquad.springcafe.domain;
+
+public class User {
+
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
