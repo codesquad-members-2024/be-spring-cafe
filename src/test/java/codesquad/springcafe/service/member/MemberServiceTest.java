faa430c89f85e128402a0ecd23d1b86d90ea8463
Yeriimii <seojs0511@naver.com>
2024-04-09 01:09:25 +0900

    chore: 테스트 클래스 이름 변경
    - MemberService 인터페이스에 대한 이름에 맞도록 접미사 `Impl` 제거

--- src/test/java/codesquad/springcafe/service/member/MemberServiceTest.java
+++ src/test/java/codesquad/springcafe/service/member/MemberServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MemberServiceImplTest {
+class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
