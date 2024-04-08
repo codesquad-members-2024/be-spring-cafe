872e2c8620c4de657612b1420e3fa94bd1acef24
Keon Kim <fnc6488@gmail.com>
2024-04-08 17:20:52 +0900

    UserController에 로거 추가
    디버깅을 위한 로거를 UserController에 추가

--- src/main/java/codesquad/springcafe/controller/UserController.java
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -2,17 +2,22 @@ package codesquad.springcafe.controller;
 
 import codesquad.springcafe.database.UserDatabase;
 import codesquad.springcafe.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class UserController {
 
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/users")
     public String getUserList() {
-        System.out.println("user list");
+        log.debug("user list");
         return "/user/list.html";
     }
 
@@ -22,10 +27,10 @@ public class UserController {
             @RequestParam("password") String password,
             @RequestParam("name") String name,
             @RequestParam("email") String email
-
     ) {
         User user = new User(userId, password, name, email);
         UserDatabase.saveUser(user);
+        log.debug("user saved: {}", user.getUserId());
         return "redirect:/user/list.html";
     }
 }
