e45d459fbcfb3c81b7d6a2c3317d5e16d2492896
yonghyeonpark <pyh4616@naver.com>
2024-04-08 20:10:13 +0900

    feat: "/users/create" url 처리
    - get
      - 회원가입 form 파일을 반환
    - post
      - user 객체를 생성 후, 회원가입 로직 진행
      - 가입 완료 후, 회원 리스트를 보여주기 위해 redirect 사용

---
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -0,0 +1,28 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.domain.User;
+import codesquad.springcafe.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@org.springframework.stereotype.Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/create")
+    public String join() {
+        return "/user/form";
+    }
+
+    @PostMapping("/users/create")
+    public String join(@ModelAttribute User user) {
+        userService.join(user);
+        return "redirect:/users";
+    }
+}
