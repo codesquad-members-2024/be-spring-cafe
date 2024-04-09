51ac7eb6b9cbcdfbe612cdbe15f3bf893da329c1
lotuxsoo <tndus5383@gmail.com>
2024-04-09 11:45:09 +0900

    feat: UserRepository 메서드 추가

--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -3,6 +3,7 @@ package codesquad.springcafe.repository;
 import codesquad.springcafe.domain.User;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,6 +13,10 @@ public class UserRepository {
     public User createUser(User user) {
         users.add(user);
         return user;
+    }
+
+    public Optional<User> findByUserId(String userId) {
+        return users.stream().filter(user -> user.getUserId().equals(userId)).findAny();
     }
 
     public void removeUser(User user) {
