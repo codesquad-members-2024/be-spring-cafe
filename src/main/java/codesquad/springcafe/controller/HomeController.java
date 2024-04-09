db0f2f1c3b01f6c972030a8c3e9590fdf8c3a0b0
zzawang <hobin0116@naver.com>
2024-04-09 11:14:39 +0900

    feat: 게시글 작성 및 조회 기능 구현
    - 사용자는 게시글을 작성할 수 있다.
    - 모든 사용자는 게시글 목록을 볼 수 있다.
    - 모든 사용자는 게시글 상세 내용을 볼 수 있다.

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,16 +1,31 @@
 package codesquad.springcafe.controller;
 
+import codesquad.springcafe.dto.Article;
+import codesquad.springcafe.service.ArticleService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    @GetMapping("/home")
-    public String homeForm() {
+    private final ArticleService articleService;
+
+    @Autowired
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping(value = {"/", "/home"})
+    public String homeForm(Model model) {
+        List<Article> articles = articleService.findAllArticle();
+        model.addAttribute("articleListSize", articles.size());
+        model.addAttribute("articleList", articles);
         return "home";
     }
 }
