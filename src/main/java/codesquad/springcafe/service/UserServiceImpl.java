574484930b3f413e459971dd95138bcb57006b06
lotuxsoo <tndus5383@gmail.com>
2024-04-09 10:59:35 +0900

    feat: UserService, UserServiceImpl 구현

---
+++ src/main/java/codesquad/springcafe/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package codesquad.springcafe.service;
+
+import codesquad.springcafe.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Override
+    public void createUser(User user) {
+
+    }
+}
