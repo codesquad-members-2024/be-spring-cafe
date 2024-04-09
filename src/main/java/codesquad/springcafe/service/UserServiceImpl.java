261374c8bfe0303a98d9ff11495327db46f8793a
lotuxsoo <tndus5383@gmail.com>
2024-04-09 11:11:16 +0900

    feat: UserRepository 구현

--- src/main/java/codesquad/springcafe/service/UserServiceImpl.java
+++ src/main/java/codesquad/springcafe/service/UserServiceImpl.java
@@ -1,12 +1,21 @@
 package codesquad.springcafe.service;
 
 import codesquad.springcafe.domain.User;
+import codesquad.springcafe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void createUser(User user) {
-
+        userRepository.createUser(user);
     }
 }
