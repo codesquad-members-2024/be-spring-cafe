163aa04a905505799d43bedba3b91f1c0cec5997
Yeriimii <seojs0511@naver.com>
2024-04-10 00:11:54 +0900

    feat: Article 기능 구현
    - 주요 필드
      - id: 식별자
      - title: 제목
      - contents: 본문
      - createdBy: 작성자
      - createdAt: 작성시간

---
+++ src/main/java/codesquad/springcafe/domain/article/Article.java
@@ -0,0 +1,78 @@
+package codesquad.springcafe.domain.article;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Article {
+    private long id;
+    private String title;
+    private String contents;
+    private String createdBy;
+    private LocalDateTime createdAt;
+
+    public Article() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Article setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Article setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public Article setContents(String contents) {
+        this.contents = contents;
+        return this;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public Article setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+        return this;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Article setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Article article)) {
+            return false;
+        }
+        return getId() == article.getId() && Objects.equals(getTitle(), article.getTitle())
+                && Objects.equals(getContents(), article.getContents()) && Objects.equals(
+                getCreatedBy(), article.getCreatedBy()) && Objects.equals(getCreatedAt(), article.getCreatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getContents(), getCreatedBy(), getCreatedAt());
+    }
+}
---
+++ src/test/java/codesquad/springcafe/domain/article/ArticleTest.java
@@ -0,0 +1,54 @@
+package codesquad.springcafe.domain.article;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ArticleTest {
+    @DisplayName("제목은 test, 작성자는 guest, 내용은 test body 인 article 객체를 생성할 수 있다")
+    @Test
+    void create() {
+        // given
+        Article article = new Article();
+
+        // when
+        article.setTitle("test");
+        article.setCreatedBy("guest");
+        article.setContents("test body");
+
+        // then
+        assertThat(article.getTitle()).isEqualTo("test");
+        assertThat(article.getCreatedBy()).isEqualTo("guest");
+        assertThat(article.getContents()).isEqualTo("test body");
+    }
+
+    @DisplayName("같은 제목, 작성자, 본문 내용, 작성시간 이더라도 발급된 id가 다르면 다른 게시물이다")
+    @Test
+    void equals() {
+        // given
+        String title = "test";
+        String createdBy = "guest";
+        String contents = "test body";
+        LocalDateTime createdAt = LocalDateTime.parse("2024-04-10T00:00:00");
+
+        // when
+        Article article1 = new Article();
+        article1.setTitle(title);
+        article1.setCreatedBy(createdBy);
+        article1.setContents(contents);
+        article1.setCreatedAt(createdAt);
+        article1.setId(1L);
+
+        Article article2 = new Article();
+        article2.setTitle(title);
+        article2.setCreatedBy(createdBy);
+        article2.setContents(contents);
+        article2.setCreatedAt(createdAt);
+        article2.setId(2L);
+
+        // then
+        assertThat(article1).isNotEqualTo(article2);
+    }
+}
