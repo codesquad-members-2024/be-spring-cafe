b0a881397639fb234aa1d5531bc54dae4602e06a
lotuxsoo <tndus5383@gmail.com>
2024-04-09 14:13:46 +0900

    fix: User 클래스 필드 변경

--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -3,7 +3,6 @@ package codesquad.springcafe.repository;
 import codesquad.springcafe.domain.User;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,10 +12,6 @@ public class UserRepository {
     public User createUser(User user) {
         users.add(user);
         return user;
-    }
-
-    public Optional<User> findByUserId(String userId) {
-        return users.stream().filter(user -> user.getUserId().equals(userId)).findAny();
     }
 
     public void removeUser(User user) {
