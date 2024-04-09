c580b707797b75b0ffde570bb0e2a7ec33f13fd7
zzawang <hobin0116@naver.com>
2024-04-09 11:14:39 +0900

    feat: 게시글 작성 및 조회 기능 구현
    - 사용자는 게시글을 작성할 수 있다.
    - 모든 사용자는 게시글 목록을 볼 수 있다.
    - 모든 사용자는 게시글 상세 내용을 볼 수 있다.

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -1,23 +1,54 @@
 package codesquad.springcafe.controller;
 
+import codesquad.springcafe.dto.Article;
+import codesquad.springcafe.service.ArticleService;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/post")
-public class PostController {
-    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+@RequestMapping("/article")
+public class ArticleController {
+    private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @GetMapping("/write")
     public String writeForm() {
-        return "post/form";
+        return "article/form";
     }
 
-    @GetMapping("/show")
-    public String showForm() {
-        return "post/show";
+    @PostMapping("/write")
+    public String write(@ModelAttribute Article article) {
+        article.setCreationTime(new Date());
+        int id = articleService.addArticle(article);
+        logger.info("[" + id + "번째 게시글 생성 완료] - " + article);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/show/{articleId}")
+    public String showForm(Model model, @PathVariable int articleId) throws Exception {
+        Article article = articleService.findArticleById(articleId - 1);
+        logger.info("[" + articleId + "번째 게시글 가져오기 성공] - " + article.toString());
+
+        int viewCount = articleService.increaseViewCount(article);
+        logger.info("[" + articleId + "번째 게시글 조회수 : " + viewCount);
+
+        model.addAttribute("article", article);
+        return "article/show";
     }
 }
