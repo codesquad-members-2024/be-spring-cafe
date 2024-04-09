394f3a35e1320878c4ed10c055de832c1caefdcc
Alex <fnelclsrn123@gmail.com>
2024-04-09 16:23:10 +0900

    feat: user 리포지토리 생성

---
+++ src/main/java/springcafe/repository/UserRepositoryImpl.java
@@ -0,0 +1,31 @@
+package springcafe.repository;
+
+import org.springframework.stereotype.Repository;
+import springcafe.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class UserRepositoryImpl implements UserRepository{
+
+    List<User> userList = new ArrayList<>();
+
+    @Override
+    public void add(User user) {
+        userList.add(user);
+    }
+
+    @Override
+    public User findById(String id) {
+        return userList.stream().filter(i-> i.isIdMatched(id))
+                .findAny()
+                .orElseThrow(()->new IllegalArgumentException("해당 id를 가진 회원이 없습니다."));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userList;
+    }
+}
