1e8e390f34bd322b4893fa2d3e1f49ca913acf91
Alex <fnelclsrn123@gmail.com>
2024-04-09 16:24:54 +0900

    feat: 회원가입 폼 생성

---
+++ src/main/java/springcafe/dto/UserCreateForm.java
@@ -0,0 +1,32 @@
+package springcafe.dto;
+
+public class UserCreateForm {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public UserCreateForm(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
