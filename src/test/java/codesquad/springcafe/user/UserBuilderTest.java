c4282eacfe0e2ec8bfad64d252eca9dea2531d42
hiidy <hiidy115@gmail.com>
2024-04-09 16:30:06 +0900

    feat(test): UserBuild 메서드를 테스하는 코드 구현

---
+++ src/test/java/codesquad/springcafe/user/UserBuilderTest.java
@@ -0,0 +1,25 @@
+package codesquad.springcafe.user;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserBuilderTest {
+
+    @Test
+    @DisplayName("빌더 패턴으로 User 객체가 생성되는지 테스트")
+    void testBuild() {
+        User user = new UserBuilder()
+            .userId("userId")
+            .email("email")
+            .nickname("nickname")
+            .password("password")
+            .build();
+
+        assertEquals("userId", user.getUserId());
+        assertEquals("email", user.getEmail());
+        assertEquals("nickname", user.getNickname());
+        assertEquals("password", user.getPassword());
+    }
+}
