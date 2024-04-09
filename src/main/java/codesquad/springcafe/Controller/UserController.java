b593e822d01a2e21429f24d345b821ee6fba34db
sean <tmdgus717@naver.com>
2024-04-09 11:10:58 +0900

    application.properties 로그 설정 추가
    logging.level.root=info
    
    logging.level.codesquad.springcafe=debug

--- src/main/java/codesquad/springcafe/Controller/UserController.java
+++ src/main/java/codesquad/springcafe/Controller/UserController.java
@@ -2,18 +2,27 @@ package codesquad.springcafe.Controller;
 
 import codesquad.springcafe.Domain.User;
 import codesquad.springcafe.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 //file : Controller
 @Controller
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    UserService userService = new UserService();
-    @GetMapping("/user/form.html")
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService; // 스프링 생성할때 생성 -> 스프링 컨테이너의 서비스와 연결
+    }
+    @GetMapping("/user/form")
     public String form() {
         return "user/form";
     }
@@ -22,9 +31,19 @@ public class UserController {
     public String users() {
         return "user/list";
     }
+
     @PostMapping("/users")
-    public String users(@ModelAttribute User user) {
+    public String users(User user) { //html의 name과 매칭
+        logger.info("info log = {}", user.getUserId());
+        logger.debug("debug log = {}", user.getEmail());
+
         userService.signUp(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/list")
+    public String userList(Model model) {
+        model.addAttribute("users", userService.findAllUser());
         return "redirect:/users";
     }
 }
--- src/main/java/codesquad/springcafe/SpringCafeApplication.java
+++ src/main/java/codesquad/springcafe/SpringCafeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCafeApplication {
+	//여기 패키지부터 하위패키지까지 스캔
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCafeApplication.class, args);
