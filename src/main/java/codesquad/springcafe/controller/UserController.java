fca350a82388c8fcfca3c4953c8aa37c3c5cd9f6
lotuxsoo <tndus5383@gmail.com>
2024-04-10 13:06:59 +0900

    chore

---
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -0,0 +1,67 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.domain.User;
+import codesquad.springcafe.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String userList(Model model) {
+        List<User> users = userRepository.findAllUsers();
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+
+    @GetMapping("/create")
+    public String createForm() {
+        return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute User user) {
+        User newUser = userRepository.createUser(user);
+        logger.info("회원가입이 성공했습니다. {}", newUser);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{userId}")
+    public String userProfile(@PathVariable("userId") String userId, Model model) {
+        Optional<User> optionalUser = userRepository.findByUserId(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            logger.info("사용자 프로필 조회 {}", user);
+            model.addAttribute("user", user);
+            return "user/profile";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "user/login";
+    }
+}
