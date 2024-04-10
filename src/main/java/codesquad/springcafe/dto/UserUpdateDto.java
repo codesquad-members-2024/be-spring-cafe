50d9cda7a2cfce38657af6b238836c4068aebf8f
yonghyeonpark <pyh4616@naver.com>
2024-04-11 03:27:01 +0900

    feat: 유저 update 정보 전송을 위한 dto 클래스 구현

---
+++ src/main/java/codesquad/springcafe/dto/UserUpdateDto.java
@@ -0,0 +1,38 @@
+package codesquad.springcafe.dto;
+
+public class UserUpdateDto {
+
+    private String password;
+    private String name;
+    private String email;
+
+    public UserUpdateDto(String password, String name, String email) {
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
