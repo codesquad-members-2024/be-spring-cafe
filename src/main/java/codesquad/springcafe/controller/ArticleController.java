0523515e91846a28fc3c46e11397f0fe6662dc7c
yonghyeonpark <pyh4616@naver.com>
2024-04-10 20:40:52 +0900

    refactor: "/" url 처리에 대한 전송 정보 추가
    - 현재 시간을 나타내는 객체를 model에 추가
    - 글 목록 속성명 지정

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -3,6 +3,7 @@ package codesquad.springcafe.controller;
 import codesquad.springcafe.domain.Article;
 import codesquad.springcafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,13 @@ public class ArticleController {
 
     @PostMapping("/questions/create")
     public String quest(@ModelAttribute Article article) {
-        articleService.saveQuestion(article);
+        articleService.saveArticle(article);
         return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String questionList(Model model) {
+        model.addAttribute("articles", articleService.getArticleList());
+        return "/index";
     }
 }
