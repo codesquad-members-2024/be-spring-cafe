5feacc807f13646982e1c4e44e6dea7ace973269
pjm2571 <pjm2571@naver.com>
2024-04-10 19:16:07 +0900

    feat: 유저 업데이트 기능 PUT 메소드로 구현 - 유저를 업데이트 하는 기능을 PUT 메소드로 구현

--- src/main/java/codesquad/springcafe/users/controller/UserController.java
+++ src/main/java/codesquad/springcafe/users/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -71,7 +68,7 @@ public class UserController {
         return "/user/updateForm";
     }
 
-    @PostMapping("/{userId}/update")
+    @PutMapping("/{userId}/update")
     public String updateUser(@PathVariable String userId, UserUpdateData updateData, Model model) {
         try {
             userService.updateUser(userId, updateData);
