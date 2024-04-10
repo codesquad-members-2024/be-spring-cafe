04fde519dd968b6eafe390ce75befc6d5ee65c46
ryudb0 <ryudb030@gmail.com>
2024-04-10 18:46:19 +0900

    UserRepository 인터페이스 구현함
    User클래스 id getter 추가함

---
+++ src/main/java/codesquad/springcafe/repository/MemoryUserRepository.java
@@ -0,0 +1,26 @@
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.domain.User;
+
+import java.util.*;
+
+public class MemoryUserRepository implements UserRepository {
+
+    private static final Map<String, User> store = new HashMap<>();
+
+    @Override
+    public User save(User user) {
+        store.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
