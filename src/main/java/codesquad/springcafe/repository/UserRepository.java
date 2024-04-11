621cb7d83434134a05faa50e55a97be6f326c468
lotuxsoo <tndus5383@gmail.com>
2024-04-11 19:18:34 +0900

    feat: UserRepositoryInterface 생성

--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -7,18 +7,21 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public class UserRepository implements UserRepositoryInterface {
     private static final List<User> users = new ArrayList<>();
 
+    @Override
     public User createUser(User user) {
         users.add(user);
         return user;
     }
 
+    @Override
     public List<User> findAllUsers() {
         return users;
     }
 
+    @Override
     public Optional<User> findByUserId(String userId) {
         return users.stream().filter(user -> user.getUserId().equals(userId)).findFirst();
     }
