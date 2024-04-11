9b430637145c637fdebb63c3e72fc1df0b2be83c
sean <tmdgus717@naver.com>
2024-04-11 23:43:49 +0900

    fix:index.html
    {{#articles}}를통해 article의 title id 를 사용하여 출력함

--- src/main/java/codesquad/springcafe/Controller/IndexController.java
+++ src/main/java/codesquad/springcafe/Controller/IndexController.java
@@ -1,14 +1,26 @@
 package codesquad.springcafe.Controller;
 
+import codesquad.springcafe.Domain.Article;
+import codesquad.springcafe.Service.ArticleService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    private final ArticleService articleService;
+
+    public IndexController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("/index")
-    public String home() {
+    public String index(Model model) {
+        List<Article> articles = articleService.findAllArticle();
+        model.addAttribute("articles", articles);
         return "index";
     }
 }
