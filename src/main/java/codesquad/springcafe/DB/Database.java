90b8d1831b03494fea9a98dd1f29a218c88ba651
seondays <agseon9@gmail.com>
2024-04-11 19:47:21 +0900

    feat : 게시글을 저장할 DB 역할 기능 추가
    게시글을 저장할 ArrayList 구현, 게시글을 추가하고 원하는 게시글을 가져올 수 있다.

--- src/main/java/codesquad/springcafe/DB/Database.java
+++ src/main/java/codesquad/springcafe/DB/Database.java
@@ -1,5 +1,6 @@
 package codesquad.springcafe.DB;
 
+import codesquad.springcafe.domain.Article;
 import codesquad.springcafe.domain.User;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.NoSuchElementException;
 
 public class Database {
     private static final List<User> users = new ArrayList<>();
+    private static final List<Article> articles = new ArrayList<>();
 
     public static void addUser(User user) {
         users.add(user);
@@ -21,5 +23,21 @@ public class Database {
                 .filter(user -> user.getUserId().equals(userId))
                 .findFirst()
                 .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static void addArticle(Article article) {
+        articles.add(article);
+    }
+
+    public static List<Article> getAllArticles() {
+        return articles;
+    }
+
+    public static int articlesSize() {
+        return articles.size();
+    }
+
+    public static Article getArticle(int index) {
+        return articles.get(index - 1);
     }
 }
