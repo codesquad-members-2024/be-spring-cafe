72f527cb279d75df5de2f50b4b00797c630aa132
sean <tmdgus717@naver.com>
2024-04-11 19:05:50 +0900

    Domain패키지에 article클래스 생성
    article 값을 저장하는 클래스생성

--- src/main/java/codesquad/springcafe/Controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/Controller/ArticleController.java
@@ -1,5 +1,16 @@
 package codesquad.springcafe.Controller;
 
+import codesquad.springcafe.Domain.Article;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("qna")
 public class ArticleController {
 
+    @PostMapping
+    public String qna(Article article) {
+
+    }
 }
---
+++ src/main/java/codesquad/springcafe/Domain/Article.java
@@ -0,0 +1,5 @@
+package codesquad.springcafe.Domain;
+
+public class Article {
+
+}
