bafadbc6a29a8ef49089270f677ae4df24a46fb5
yonghyeonpark <pyh4616@naver.com>
2024-04-11 21:21:14 +0900

    refactor: userId를 불변으로 설정
    - userId에 final 키워드를 붙여 불변으로 설정
    - userId setter 함수 제거

--- src/main/java/codesquad/springcafe/domain/user/User.java
+++ src/main/java/codesquad/springcafe/domain/user/User.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class User {
 
-    private String userId;
+    private final String userId;
     private String password;
     private String name;
     private String email;
@@ -26,10 +26,6 @@ public class User {
 
     public String getUserId() {
         return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 
     public String getPassword() {
