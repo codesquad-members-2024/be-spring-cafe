0fa61e7cc314f86b035814a73d330ee6bd14dd85
Yeriimii <seojs0511@naver.com>
2024-04-11 14:02:33 +0900

    feat: HomeController 기능 구현
    - 데이터베이스에 존재하는 게시물을 게시판에 보여줄 수 있다

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,13 +1,25 @@
 package codesquad.springcafe.controller;
 
+import codesquad.springcafe.domain.article.Article;
+import codesquad.springcafe.service.article.ArticleManager;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final ArticleManager articleManager;
+
+    public HomeController(ArticleManager articleManager) {
+        this.articleManager = articleManager;
+    }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Article> articles = articleManager.findAllArticle();
+        model.addAttribute("articles", articles);
+
         return "home";
     }
 }
