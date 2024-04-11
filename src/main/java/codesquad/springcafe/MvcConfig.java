b3ac00b4a7f0ac5c646c5e80960ac4da202e2b47
sean <tmdgus717@naver.com>
2024-04-11 18:13:57 +0900

    MvcConfig 구현
    user/form 과 user/login 같은 static한 html 파일을 보여주는 컨트롤러를 구현하지 않기 위해
    WebMvcConfigurer의 addViewControllers 를 통해 특정경로로 들어온 요청의 뷰를 바로 보여준다
    setOrder(Ordered.HIGHEST_PRECEDENCE) 를 통해 우선순위를 설정함

---
+++ src/main/java/codesquad/springcafe/MvcConfig.java
@@ -0,0 +1,20 @@
+package codesquad.springcafe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    //addViewControllers 메서드는 일반적으로 간단한 URL 경로를 컨트롤러 클래스나 메서드에 매핑하지 않고,
+    // 특정 URL 경로를 바로 뷰로 연결할 때 사용됩니다.
+    // 즉, 이 메서드를 사용하여 컨트롤러 클래스나 메서드를 정의하지 않고도 특정 URL 경로로 요청이 들어왔을 때 바로 뷰를 보여줄 수 있습니다.
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE); // 빈을 가장 먼저 찾도록 설정
+        registry.addViewController("/users/form").setViewName("user/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+    }
+}
