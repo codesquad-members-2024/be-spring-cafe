98cea9c654c8ee43d9a9bc7ef137bdf308a7a1d2
Miensoap <miensop51@gmail.com>
2024-04-12 01:16:25 +0900

    `/qna` url 변경 : `/article`

--- src/main/java/codesquad/springcafe/article/ArticleController.java
+++ src/main/java/codesquad/springcafe/article/ArticleController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/qna")
+@RequestMapping("/article")
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
