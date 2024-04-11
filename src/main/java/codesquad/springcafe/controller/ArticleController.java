63cd72c1918e4962451a9f37eeab6727c5d60261
seondays <agseon9@gmail.com>
2024-04-11 19:45:55 +0900

    feat : 게시글 작성에 관련된 url 요청을 처리할 controller 구현
    게시글을 작성하는 화면 보여줌, 실제 게시글을 작성하는 요청, 상세 게시글을 보여주는 요청을 처리할 코드 작성

---
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -0,0 +1,35 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.DB.Database;
+import codesquad.springcafe.domain.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    private final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    @GetMapping(value = {"/qna", "/qna/form.html"})
+    public String showForm() {
+        return "qna/form";
+    }
+
+    @PostMapping(value = {"/qna", "/qna/form.html"})
+    public String register(Article article) {
+        Database.addArticle(article);
+        article.setId(Database.articlesSize());
+        logger.debug("new article: " + article);
+        return "redirect:/";
+    }
+
+    @GetMapping("/article/{articleId}")
+    public String showArticle(Model model, @PathVariable("articleId") String articleId) {
+        model.addAttribute("article", Database.getArticle(Integer.parseInt(articleId)));
+        return "qna/show";
+    }
+}
