15dad84532c5062da8c79eeb83364fa200e5d6f2
hiidy <hiidy115@gmail.com>
2024-04-11 11:13:55 +0900

    feat: 질문 게시글에 대한 정보를 담은 Article 클래스 생성
    - 글쓴이, 제목, 내용에 대한 정보를 담고 있습니다.

---
+++ src/main/java/codesquad/springcafe/article/Article.java
@@ -0,0 +1,26 @@
+package codesquad.springcafe.article;
+
+public class Article {
+
+    private String title;
+    private String author;
+    private String content;
+
+    public Article(String title, String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
