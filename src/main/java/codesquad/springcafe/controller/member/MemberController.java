36ed2a1dfd0101a4af6dbd98a59492d7a9c70671
Yeriimii <seojs0511@naver.com>
2024-04-11 22:51:01 +0900

    feat: MemberController 기능 구현
    - 회원 정보 수정 기능 구현

--- src/main/java/codesquad/springcafe/controller/member/MemberController.java
+++ src/main/java/codesquad/springcafe/controller/member/MemberController.java
@@ -4,15 +4,20 @@ import codesquad.springcafe.domain.member.Member;
 import codesquad.springcafe.service.member.MemberService;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -64,6 +69,53 @@ public class MemberController {
         model.addAttribute("member", responseMember);
 
         return "members/profile";
+    }
+
+    @GetMapping("/{loginId}/update")
+    public String updateForm(@PathVariable("loginId") String loginId, @ModelAttribute("form") UpdateMember form) {
+        Optional<Member> findMember = memberService.findMember(loginId);
+
+        if (findMember.isEmpty()) {
+            return "error/404";
+        }
+
+        Member member = findMember.get();
+        fillForm(form, member);
+
+        return "members/updateForm";
+    }
+
+    @PutMapping("/{loginId}")
+    public String update(@PathVariable("loginId") String loginId,
+                         @Validated @ModelAttribute("form") UpdateMember form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/updateForm";
+        }
+
+        Optional<Member> optionalMember = memberService.findMember(loginId);
+
+        /* 존재하지 않는 회원일 때 */
+        if (optionalMember.isEmpty()) {
+            bindingResult.addError(new ObjectError("form", "존재하지 않는 회원입니다."));
+            return "members/updateForm";
+        }
+
+        /* 기존 비밀번호와 일치하지 않을 때 */
+        Member findMember = optionalMember.get();
+        if (!memberService.validatePassword(findMember, form.getBeforePassword())) {
+            bindingResult.addError(new FieldError("form", "beforePassword", "기존 비밀번호를 잘못 입력하셨습니다. 다시 확인해주세요."));
+            return "members/updateForm";
+        }
+
+        /* 정상 로직 */
+        memberService.update(loginId, form);
+        return "redirect:/members/{loginId}";
+    }
+
+    private void fillForm(UpdateMember form, Member member) {
+        form.setLoginId(member.getLoginId());
+        form.setUserName(member.getUserName());
+        form.setEmail(member.getEmail());
     }
 
     private ResponseMember convertToDTO(Member member) {
