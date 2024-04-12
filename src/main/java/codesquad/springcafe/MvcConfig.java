48e15ddbc31308f11971d3e290a61bee5c1b371e
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-04-12 20:03:09 +0900

    feat: html 중복 제거를 위해 qna폴더를 static에서 template 로 이동

--- src/main/java/codesquad/springcafe/MvcConfig.java
+++ src/main/java/codesquad/springcafe/MvcConfig.java
@@ -16,6 +16,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/user/form").setViewName("user/form");
         registry.addViewController("/user/login").setViewName("user/login");
+        registry.addViewController("/qna/form").setViewName("qna/form");
     }
 }
 
