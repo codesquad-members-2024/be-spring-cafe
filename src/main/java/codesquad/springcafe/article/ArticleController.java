033aee851f2b154c6a720a539796835363622075
hiidy <hiidy115@gmail.com>
2024-04-12 11:46:34 +0900

    refactor: 게시글 Controller의 기본 URL을 설정

--- src/main/java/codesquad/springcafe/article/ArticleController.java
+++ src/main/java/codesquad/springcafe/article/ArticleController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/article")
 public class ArticleController {
 
     ArticleDatabase articleDatabase;
@@ -16,18 +18,18 @@ public class ArticleController {
     }
 
 
-    @GetMapping("/article/form")
+    @GetMapping("/form")
     public String articleForm() {
         return "article/form";
     }
 
-    @PostMapping("/article/create")
+    @PostMapping("/create")
     public String createArticle(ArticleCreateDto articleCreateDto) {
         articleDatabase.saveArticle(articleCreateDto.toEntity());
         return "redirect:/";
     }
 
-    @GetMapping("/article/{articleId}")
+    @GetMapping("/{articleId}")
     public String showArticle(@PathVariable long articleId, Model model) {
         model.addAttribute("article", articleDatabase.findById(articleId));
         return "article/show";
