99a28e2d7506dd7c8e8f1ed605ed66cb0fcc2dba
hiidy <hiidy115@gmail.com>
2024-04-12 15:19:33 +0900

    refactor: 기존 저장소를 InMemoryDatabase로 클래스명 변경

---
+++ src/main/java/codesquad/springcafe/article/ArticleInMemoryDatabase.java
@@ -0,0 +1,33 @@
+package codesquad.springcafe.article;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ArticleInMemoryDatabase {
+
+    private final List<Article> articles = new ArrayList<>();
+
+    public void saveArticle(Article article) {
+        article.setArticleId(articles.size() + 1);
+        articles.add(article);
+    }
+
+    public List<Article> findAll() {
+        return Collections.unmodifiableList(articles);
+    }
+
+    public Article findById(long id) {
+        return articles.stream()
+            .filter(article -> article.getArticleId() == id)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("해당 아이디의 게시글이 없습니다."));
+    }
+
+    public void clear() {
+        articles.clear();
+    }
+}
