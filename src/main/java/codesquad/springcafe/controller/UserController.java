2c35babe9e89fff06857b21bbd1e0a74f8e88c09
ehpark <silbroeh@gmail.com>
2024-04-12 11:09:38 +0900

    feat : 회원가입 기능
    1. /user/form.html 파일 수정
    - templates 디렉토리 이동
    - form 태그: method 변경(get -> post)
    2. controller 기능
    - post url 매핑
    - user 데이터 바인딩 및 model 객체 데이터 전달
    - 사용자 목록 페이지 리다이렉트

---
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -0,0 +1,27 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.model.User;
+import codesquad.springcafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public String signUp(@ModelAttribute("user") User user, Model model) {
+        User newUser = userService.createUser(user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        model.addAttribute("user", newUser);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/codesquad/springcafe/service/UserService.java
@@ -0,0 +1,18 @@
+package codesquad.springcafe.service;
+
+import codesquad.springcafe.model.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final List<User> users = new ArrayList<>();
+
+    public User createUser(String userId, String password, String name, String email) {
+        User newUser = new User(userId, password, name, email);
+        users.add(newUser);
+        return newUser;
+    }
+}
