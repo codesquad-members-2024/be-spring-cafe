6c39f4f703b6167b4c78f0c55ba64172492e10ca
hiidy <hiidy115@gmail.com>
2024-04-12 11:29:59 +0900

    refactor: default url을 /user로 설정하고 리팩터링

--- src/main/java/codesquad/springcafe/user/UserController.java
+++ src/main/java/codesquad/springcafe/user/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     UserDatabase userDatabase;
@@ -17,34 +19,34 @@ public class UserController {
     }
 
     // 유저 등록 페이지
-    @GetMapping("/user/form")
+    @GetMapping("/form")
     public String form() {
         return "user/form";
     }
 
     // 유저 등록 메서드
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     public String register(UserSignupDto userSignupDto) {
         userDatabase.save(userSignupDto.toEntity());
         return "redirect:/";
     }
 
     // 유저 목록 페이지
-    @GetMapping("/user")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userDatabase.findAll());
         return "user/list";
     }
 
     // 유저 프로필 페이지
-    @GetMapping("/user/{userId}")
+    @GetMapping("/{userId}")
     public String profile(@PathVariable String userId, Model model) {
         model.addAttribute("user", userDatabase.findByUserId(userId));
         return "user/profile";
     }
 
     // 유저 정보 수정 페이지
-    @GetMapping("/user/{userId}/form")
+    @GetMapping("{userId}/form")
     public String updateProfilePage(@PathVariable String userId, Model model) {
         model.addAttribute("user", userDatabase.findByUserId(userId));
         return "user/update_form";
@@ -52,7 +54,7 @@ public class UserController {
 
 
     //수정된 회원 정보 post
-    @PutMapping("/user/{userId}/form")
+    @PutMapping("{userId}/form")
     public String putUpdateProfile(@PathVariable String userId, UserUpdateDto userUpdateDto) {
         User foundUser = userDatabase.findByUserId(userId);
         if (foundUser.checkPassword(userUpdateDto.getPassword())) {
