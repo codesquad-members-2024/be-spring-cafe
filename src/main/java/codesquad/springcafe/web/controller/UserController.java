5c66216f300b7c00826117ee1ef6b2732f972db4
yonghyeonpark <pyh4616@naver.com>
2024-04-14 01:14:47 +0900

    refactor: 유저 updateForm 검증 처리
    - (Get, /users/{userId}/update)
      - 동일한 form에서 처리하기 위해 빈 UserUpdateDto를 Model에 담습니다.
    - (Post, /users/{userId}/update)
      - form의 필드 값 검증 처리
        - 모든 필드에 공백을 허용하지 않습니다.
        - 기존의 비밀번호가 올바른지 검증합니다.
        - 새 비밀번호와 그에 대한 확인이 일치하는지 검증합니다.
      - 에러가 존재할 때, 다시 form으로 돌아가야 하는데, 이 떄, userId가
        필요하므로 다시 Model에 담아줍니다.

--- src/main/java/codesquad/springcafe/web/controller/UserController.java
+++ src/main/java/codesquad/springcafe/web/controller/UserController.java
@@ -73,12 +73,45 @@ public class UserController {
 
     @GetMapping("/{userId}/update")
     public String userUpdate(@PathVariable String userId, Model model) {
-        model.addAttribute("user", userService.findOne(userId));
+        model.addAttribute("userId", userId);
+        model.addAttribute("update", new UserUpdateDto());
         return "/user/updateForm";
     }
 
     @PutMapping("/{userId}/update")
-    public String userUpdate(@PathVariable String userId, @ModelAttribute UserUpdateDto userUpdateDto) {
+    public String userUpdate(@PathVariable String userId, @ModelAttribute("update") UserUpdateDto userUpdateDto, BindingResult bindingResult, Model model) {
+        if (!StringUtils.hasText(userUpdateDto.getCurrentPassword())) {
+            bindingResult.addError(new FieldError("update", "currentPassword", userUpdateDto.getCurrentPassword(),
+                    false, null, null, "현재 비밀번호를 입력하세요."));
+        }
+        if (!StringUtils.hasText(userUpdateDto.getNewPassword())) {
+            bindingResult.addError(new FieldError("update", "newPassword", userUpdateDto.getNewPassword(),
+                    false, null, null, "새 비밀번호를 입력하세요."));
+        }
+        if (!StringUtils.hasText(userUpdateDto.getNewPasswordCheck())) {
+            bindingResult.addError(new FieldError("update", "newPasswordCheck", userUpdateDto.getNewPasswordCheck(),
+                    false, null, null, "새 비밀번호 확인을 입력하세요."));
+        }
+        if (!StringUtils.hasText(userUpdateDto.getName())) {
+            bindingResult.addError(new FieldError("update", "name", userUpdateDto.getName(),
+                    false, null, null, "이름을 입력하세요."));
+        }
+        if (!StringUtils.hasText(userUpdateDto.getEmail())) {
+            bindingResult.addError(new FieldError("update", "email", userUpdateDto.getEmail(),
+                    false, null, null, "이메일을 입력하세요."));
+        }
+        if (!userService.findOne(userId).getPassword().equals(userUpdateDto.getCurrentPassword())) {
+            bindingResult.addError(new FieldError("update", "currentPassword", userUpdateDto.getCurrentPassword(),
+                    false, null, null, "비밀번호가 일치하지 않습니다."));
+        }
+        if (!userUpdateDto.getNewPassword().equals(userUpdateDto.getNewPasswordCheck())) {
+            bindingResult.addError(new FieldError("update", "newPasswordCheck", userUpdateDto.getCurrentPassword(),
+                    false, null, null, "비밀번호가 일치하지 않습니다."));
+        }
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("userId", userId);
+            return "/user/updateForm";
+        }
         userService.updateUser(userId, userUpdateDto);
         return "redirect:/users";
     }
