6e772617575f2d4e84ab3cb13bdb358865469ce9
ohjuyoungg <ejrm12@naver.com>
2024-04-14 05:24:14 +0900

    feat: UserRepository 클래스 구현

--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -1,2 +1,30 @@
-package codesquad.springcafe.repository;public class UserRepository {
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public Optional<User> findUserById(String userId) {
+        // userId에 해당하는 사용자를 찾아서 Optional로 감싸서 반환
+        for (User user : users) {
+            if (user.getUserId().equals(userId)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty(); // 해당 userId를 가진 사용자가 없을 경우 empty Optional을 반환
+    }
+
+    public Collection<User> findAll() {
+        return users;
+    }
 }
