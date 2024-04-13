52d8e2f17289c8a217e9f8a4b8e27bf59d5906c7
lotuxsoo <tndus5383@gmail.com>
2024-04-13 14:42:09 +0900

    feat: HomeController에 게시글 메서드 추가

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,13 +1,26 @@
 package codesquad.springcafe.controller;
 
+import codesquad.springcafe.domain.Article;
+import codesquad.springcafe.repository.article.ArticleRepositoryInterface;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final ArticleRepositoryInterface articleRepository;
+
+    @Autowired
+    public HomeController(ArticleRepositoryInterface articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        List<Article> articles = articleRepository.findAllArticles();
+        model.addAttribute("articles", articles);
         return "index";
     }
 }
