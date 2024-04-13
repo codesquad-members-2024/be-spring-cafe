97463c1d91bed4a6975258a56907e0b6d3b90f06
lotuxsoo <tndus5383@gmail.com>
2024-04-13 14:27:14 +0900

    feat: ArticleRepository 구현

--- src/main/java/codesquad/springcafe/repository/article/ArticleRepository.java
+++ src/main/java/codesquad/springcafe/repository/article/ArticleRepository.java
@@ -3,11 +3,26 @@ package codesquad.springcafe.repository.article;
 import codesquad.springcafe.domain.Article;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ArticleRepository implements ArticleRepositoryInterface {
     private static final List<Article> articles = new ArrayList<>();
 
+    @Override
+    public Article createArticle(Article article) {
+        articles.add(article);
+        return article;
+    }
 
+    @Override
+    public List<Article> findAllArticles() {
+        return articles;
+    }
+
+    @Override
+    public Optional<Article> findByArticleId(Long articleId) {
+        return articles.stream().filter(article -> article.getArticleId().equals(articleId)).findFirst();
+    }
 }
