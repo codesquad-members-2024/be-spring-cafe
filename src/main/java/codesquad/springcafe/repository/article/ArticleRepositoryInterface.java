ca4ab46d9208f036f2ebd78f40bec96f65904c30
lotuxsoo <tndus5383@gmail.com>
2024-04-13 14:27:14 +0900

    feat: ArticleRepository 구현

--- src/main/java/codesquad/springcafe/repository/article/ArticleRepositoryInterface.java
+++ src/main/java/codesquad/springcafe/repository/article/ArticleRepositoryInterface.java
@@ -1,5 +1,13 @@
 package codesquad.springcafe.repository.article;
 
-public interface ArticleRepositoryInterface {
+import codesquad.springcafe.domain.Article;
+import java.util.List;
+import java.util.Optional;
 
+public interface ArticleRepositoryInterface {
+    Article createArticle(Article article);
+
+    List<Article> findAllArticles();
+
+    Optional<Article> findByArticleId(Long articleId);
 }
