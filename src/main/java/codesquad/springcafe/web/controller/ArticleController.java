95fe47de098b36bf05f68ca83a06c550d2fdab99
yonghyeonpark <pyh4616@naver.com>
2024-04-14 04:46:56 +0900

    refactor: ArticleController의 검증 로직 분리
    - ArticleCreateDto 타입의 객체를 검증하는 ArticleCreateValidator를
      구현하여 분리

--- src/main/java/codesquad/springcafe/web/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/web/controller/ArticleController.java
@@ -2,23 +2,28 @@ package codesquad.springcafe.web.controller;
 
 import codesquad.springcafe.service.ArticleService;
 import codesquad.springcafe.web.dto.ArticleCreateDto;
+import codesquad.springcafe.web.validation.ArticleCreateValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final ArticleCreateValidator articleCreateValidator;
 
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, ArticleCreateValidator articleCreateValidator) {
         this.articleService = articleService;
+        this.articleCreateValidator = articleCreateValidator;
+    }
+
+    @InitBinder("article")
+    public void initTargetCreate(WebDataBinder dataBinder) {
+        dataBinder.addValidators(articleCreateValidator);
     }
 
     @GetMapping("/articles/create")
@@ -28,19 +33,7 @@ public class ArticleController {
     }
 
     @PostMapping("/articles/create")
-    public String quest(@ModelAttribute("article") ArticleCreateDto articleCreateDto, BindingResult bindingResult) {
-        if (!StringUtils.hasText(articleCreateDto.getWriter())) {
-            bindingResult.addError(new FieldError("article", "writer", articleCreateDto.getWriter(),
-                    false, null, null, "작성자를 입력하세요."));
-        }
-        if (!StringUtils.hasText(articleCreateDto.getTitle())) {
-            bindingResult.addError(new FieldError("article", "title", articleCreateDto.getTitle(),
-                    false, null, null, "제목을 입력하세요."));
-        }
-        if (!StringUtils.hasText(articleCreateDto.getContents())) {
-            bindingResult.addError(new FieldError("article", "contents", articleCreateDto.getContents(),
-                    false, null, null, "내용을 입력하세요."));
-        }
+    public String quest(@Validated @ModelAttribute("article") ArticleCreateDto articleCreateDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "/qna/form";
         }
---
+++ src/main/java/codesquad/springcafe/web/validation/ArticleCreateValidator.java
@@ -0,0 +1,31 @@
+package codesquad.springcafe.web.validation;
+
+import codesquad.springcafe.web.dto.ArticleCreateDto;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ArticleCreateValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return ArticleCreateDto.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ArticleCreateDto articleCreateDto = (ArticleCreateDto) target;
+
+        if (!StringUtils.hasText(articleCreateDto.getWriter())) {
+            errors.rejectValue("writer", "temporary", null, "작성자를 입력하세요.");
+        }
+        if (!StringUtils.hasText(articleCreateDto.getTitle())) {
+            errors.rejectValue("title", "temporary", null, "제목을 입력하세요.");
+        }
+        if (!StringUtils.hasText(articleCreateDto.getContents())) {
+            errors.rejectValue("contents", "temporary", null, "내용을 입력하세요.");
+        }
+    }
+}
