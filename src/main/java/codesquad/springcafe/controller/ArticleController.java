bccd41eaad9e57496b803c61a65f57a40682cfed
lotuxsoo <tndus5383@gmail.com>
2024-04-13 12:51:59 +0900

    feat: 로그인 기능 구현

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -5,8 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/articles")
 public class ArticleController {
     private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
     private final ArticleRepositoryInterface articleRepository;
@@ -14,6 +18,21 @@ public class ArticleController {
     @Autowired
     public ArticleController(ArticleRepositoryInterface articleRepository) {
         this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/create")
+    public String getArticleForm() {
+        return "article/create";
+    }
+
+    @PostMapping("/create")
+    public void postArticle() {
+        //글쓰기 기능
+    }
+
+    @GetMapping("/{articleId}")
+    public String showArticle() {
+        return "article/show";
     }
 
 }
