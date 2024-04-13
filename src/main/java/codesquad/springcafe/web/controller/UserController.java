dee173546905c57b67b5ecb5d653fd611295c323
yonghyeonpark <pyh4616@naver.com>
2024-04-13 18:24:41 +0900

    refactor: 회원가입 form 검증 로직 추가
    - 하나의 form에서 입력과 에러를 처리하기 위해 (Get, "/users/create")에서
      Model에 UserCreateDto 빈 객체를 담아서 넘겨줍니다.
    - (Post, "/users/create)에 대한 처리에서는 form의 모든 항목들에 공백을
      허용하지 않고, 비밀번호와 비밀번호 확인 필드가 일치하는지를
      판단하여 FieldError 객체를 추가합니다.
    - BindingResult에 에러가 담겨 있을 경우, 작성중인 form으로 다시
      돌아갑니다. 이 때, rejectedValue의 값을 넘어온 데이터로 설정해놨기
      때문에, 작성했던 항목이 그대로 보존됩니다.

--- src/main/java/codesquad/springcafe/web/controller/UserController.java
+++ src/main/java/codesquad/springcafe/web/controller/UserController.java
@@ -1,11 +1,15 @@
 package codesquad.springcafe.web.controller;
 
-import codesquad.springcafe.domain.user.User;
 import codesquad.springcafe.service.UserService;
+import codesquad.springcafe.web.dto.UserCreateDto;
 import codesquad.springcafe.web.dto.UserUpdateDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -18,13 +22,41 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("user", new UserCreateDto());
         return "/user/form";
     }
 
     @PostMapping("/create")
-    public String join(@ModelAttribute User user) {
-        userService.join(user);
+    public String join(@ModelAttribute("user") UserCreateDto userCreateDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (!StringUtils.hasText(userCreateDto.getUserId())) {
+            bindingResult.addError(new FieldError("user", "userId", userCreateDto.getUserId(),
+                    false, null, null, "아이디를 입력하세요."));
+        }
+        if (!StringUtils.hasText(userCreateDto.getPassword())) {
+            bindingResult.addError(new FieldError("user", "password", userCreateDto.getPassword(),
+                    false, null, null, "비밀번호를 입력하세요."));
+        }
+        if (!StringUtils.hasText(userCreateDto.getPasswordCheck())) {
+            bindingResult.addError(new FieldError("user", "passwordCheck", userCreateDto.getPasswordCheck(),
+                    false, null, null, "비밀번호 확인을 입력하세요."));
+        }
+        if (!userCreateDto.getPassword().equals(userCreateDto.getPasswordCheck())) {
+            bindingResult.addError(new FieldError("user", "passwordCheck", userCreateDto.getPasswordCheck(),
+                    false, null, null, "비밀번호가 일치하지 않습니다."));
+        }
+        if (!StringUtils.hasText(userCreateDto.getName())) {
+            bindingResult.addError(new FieldError("user", "name", userCreateDto.getName(),
+                    false, null, null, "사용자 이름을 입력하세요."));
+        }
+        if (!StringUtils.hasText(userCreateDto.getEmail())) {
+            bindingResult.addError(new FieldError("user", "email", userCreateDto.getEmail(),
+                    false, null, null, "이메일을 입력하세요."));
+        }
+        if (bindingResult.hasErrors()) {
+            return "/user/form";
+        }
+        userService.join(userCreateDto);
         return "redirect:/users";
     }
 
@@ -34,7 +66,7 @@ public class UserController {
         return "/user/list";
     }
 
-    @GetMapping("/{userId}") // /users/bbb
+    @GetMapping("/{userId}")
     public String userDetails(@PathVariable String userId, Model model) {
         model.addAttribute("user", userService.findOne(userId));
         return "/user/profile";
