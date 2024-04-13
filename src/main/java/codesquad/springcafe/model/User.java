b043978c9ff857b7f3a84fa43d97ab39e57c2070
ohjuyoungg <ejrm12@naver.com>
2024-04-14 05:22:55 +0900

    feat: model 패키지내에 User 클래스 구현

--- src/main/java/codesquad/springcafe/model/User.java
+++ src/main/java/codesquad/springcafe/model/User.java
@@ -1,2 +1,52 @@
-package codesquad.springcafe.model;public class User {
+package codesquad.springcafe.model;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+    }
 }
