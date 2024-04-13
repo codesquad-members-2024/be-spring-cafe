18119d53101569bcf2f3ef38a25f085db26c0733
choejieun <wldms5221@icloud.com>
2024-04-13 14:50:17 +0900

    MemberService 회원 관리 기능 구현

---
+++ src/main/java/codesquad/springcafe/service/MemberService.java
@@ -0,0 +1,41 @@
+package codesquad.springcafe.service;
+
+import codesquad.springcafe.controller.MemberForm;
+import codesquad.springcafe.domain.Member;
+import codesquad.springcafe.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setEmail(memberForm.getEmail());        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
