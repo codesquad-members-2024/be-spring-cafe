4d9c1d13a4e7ce5fa5b4af175bfc9fa7b4b3206f
Miensoap <miensop51@gmail.com>
2024-04-14 21:41:58 +0900

    DB 초기화 쿼리 설정

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USERS(USERID, PASSWORD, NAME, EMAIL) VALUES ('guest', 'guestPassword', '익명', '게스트');
+INSERT INTO USERS(USERID, PASSWORD, NAME, EMAIL) VALUES ('tester', '1234', '테스터', '테스터');
