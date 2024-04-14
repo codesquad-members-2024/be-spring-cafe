020bffbffc70870b33ad0f91db37bbbfc637c4eb
lotuxsoo <tndus5383@gmail.com>
2024-04-14 13:13:49 +0900

    fix: ArticleRepository의 내부 자료구조를 ConcurrentHashMap으로 변경

---
+++ src/main/java/codesquad/springcafe/dto/ArticleDto.java
@@ -0,0 +1,29 @@
+package codesquad.springcafe.dto;
+
+import codesquad.springcafe.domain.Article;
+import java.time.LocalDateTime;
+
+public class ArticleDto {
+    private String title;
+    private String description;
+    private String writer;
+    private LocalDateTime localDateTime;
+    private long views;
+
+    public ArticleDto(String title, String description) {
+        this.title = title;
+        this.description = description;
+        /* 나머지 필드 초기화 */
+        this.writer = "익명";
+        this.localDateTime = LocalDateTime.now();
+        this.views = 0L;
+    }
+
+    public Article toEntity(Long articleId) {
+        return new Article(articleId, writer, title, description, localDateTime, views);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
