1cafe0d4d9f8749673adef4b5a504ef134313cc5
Daniel <923daniel@naver.com>
2024-04-15 08:50:07 +0900

    refactor: Article 클래스에 id 필드 추가

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -1,2 +1,26 @@
-package codesquad.springcafe.controller;public class ArticleController {
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.DB.ArticleDatabase;
+import codesquad.springcafe.domain.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    @PostMapping("/article")
+    // @ModelAttribute 어노테이션 을 통해 Post body를 파싱해 article 객체로 반환한다.
+    public String saveArticle(@ModelAttribute Article article) {
+        article.setId(Integer.toString(ArticleDatabase.getArticleSize()+1)); // article id 설정
+        ArticleDatabase.saveArticle(article);
+        logger.debug("new article: " + article.toString());
+
+        // article를 저장 후 main 페이지로 redirect
+        return "redirect:/main";
+    }
+
 }
--- src/main/java/codesquad/springcafe/domain/Article.java
+++ src/main/java/codesquad/springcafe/domain/Article.java
@@ -7,6 +7,7 @@ public class Article {
     private String title;
     private String content;
     private LocalDateTime time;
+    private String id;
 
 
     public Article(String writer, String title, String content) {
@@ -16,26 +17,34 @@ public class Article {
         this.time = LocalDateTime.now();
     }
 
-    public String getWriter() {
+    public String getWriter(){
         return writer;
     }
 
-    public String getTitle() {
+    public String getTitle(){
         return title;
     }
 
-    public String getContent() {
+    public String getContent(){
         return content;
     }
 
-    public LocalDateTime getTime() {
+    public LocalDateTime getTime(){
         return time;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
     }
 
 
     @Override
     public String toString() {
-        return "writer: " + writer + " title: " + title + " content: " + content;
+        return "[" + "id" + "] " + "writer: " + writer + ", title: " + title + ", content: " + content;
     }
     
 }
