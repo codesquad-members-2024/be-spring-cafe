f6d90e9df60be5c3b6126b2ee865dc321e48c1ee
KDT live <dnjsxo0616@gmail.com>
2024-04-15 12:05:05 +0900

    feat: UserService 클래스 추가
    - UserService 클래스를 추가하여 사용자 등록, 사용자 조회 기능을 구현

---
+++ src/main/java/codesquad/springcafe/web/service/UserService.java
@@ -0,0 +1,39 @@
+package codesquad.springcafe.web.service;
+
+import codesquad.springcafe.web.repository.UserRepository;
+import codesquad.springcafe.web.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String join(User user) {
+
+        validateDuplicateUser(user);
+        userRepository.save(user);
+        return user.getUserId();
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUser(String userName) {
+        return userRepository.findByName(userName);
+    }
+
+    private void validateDuplicateUser(User user) {
+        userRepository.findByName(user.getName())
+                        .ifPresent(u -> {
+                            throw new IllegalArgumentException("이미 존재하는 유저입니다.");
+                        });
+    }
+
+}
