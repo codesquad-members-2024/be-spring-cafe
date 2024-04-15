338a3a7fed39e18ee1e9b48685acb512666f9998
Daniel <923daniel@naver.com>
2024-04-16 00:57:11 +0900

    refactor: update_form.html 구현, url 맴핑

---
+++ src/main/java/codesquad/springcafe/domain/UserUpdate.java
@@ -0,0 +1,27 @@
+package codesquad.springcafe.domain;
+
+public class UserUpdate {
+    private final String newNickname;
+    private final String newEmail;
+    private final String password;
+    private final String newPassword;
+
+    public UserUpdate(String newNickname, String newEmail, String password, String newPassword) {
+        this.newNickname = newNickname;
+        this.newEmail = newEmail;
+        this.password = password;
+        this.newPassword = newPassword;
+    }
+    public String getNewNickname() {
+        return newNickname;
+    }
+
+    public String getNewEmail() {
+        return newEmail;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+}
