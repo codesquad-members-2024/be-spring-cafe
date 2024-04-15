a46a28a34a5375bcb4a7a32624fd87d1644099d2
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-15 11:36:44 +0900

    feat: 회원 목록 페이지 get 요청시 핸들러 구현

--- src/main/java/codesquad/springcafe/UserController.java
+++ src/main/java/codesquad/springcafe/UserController.java
@@ -2,8 +2,12 @@ package codesquad.springcafe;
 
 import codesquad.springcafe.db.UserDatabase;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -13,4 +17,12 @@ public class UserController {
         UserDatabase.addUser(user);
         return "redirect:/users";
     }
+
+    @GetMapping("/users")
+    public String userList(Model model){
+        List<User> users = UserDatabase.findAllUsers();
+        model.addAttribute("users", users);
+        return "users/list";
+    }
+
 }
