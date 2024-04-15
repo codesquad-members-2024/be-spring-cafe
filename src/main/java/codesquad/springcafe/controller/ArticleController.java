46d3a1c25926245d5cd44c312f22c3425ed7c71f
Daniel <923daniel@naver.com>
2024-04-15 11:03:24 +0900

    refactor: main.html에 글 목록 표시 기능 구현

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -1,20 +1,31 @@
 package codesquad.springcafe.controller;
 
 import codesquad.springcafe.DB.ArticleDatabase;
+import codesquad.springcafe.DB.UserDatabase;
 import codesquad.springcafe.domain.Article;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
     private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
+    @GetMapping("/main")
+    public String showArticle(Model model) {
+        model.addAttribute("articles", ArticleDatabase.getAllArticles()); // 전체 article 반환
+
+        return "main";
+    }
+
     @PostMapping("/article")
     // @ModelAttribute 어노테이션 을 통해 Post body를 파싱해 article 객체로 반환한다.
-    public String saveArticle(@ModelAttribute Article article) {
+    public String saveArticle(@ModelAttribute Article article, Model model) {
         article.setId(Integer.toString(ArticleDatabase.getArticleSize()+1)); // article id 설정
         ArticleDatabase.saveArticle(article);
         logger.debug("new article: " + article.toString());
@@ -22,5 +33,19 @@ public class ArticleController {
         // article를 저장 후 main 페이지로 redirect
         return "redirect:/main";
     }
+
+//    @GetMapping("/detail/{id}")
+//    public String showArticle(@PathVariable Long id, Model model) {
+//        Optional<Article> optionalArticle = articleDatabase.findBy(id);
+//        if (optionalArticle.isEmpty()) {
+//            return "redirect:/";
+//        }
+//        Article article = optionalArticle.get();
+//        article.increaseViews();
+//        model.addAttribute("article", article);
+//
+//        return "article/show";
+//    }
+
 
 }
