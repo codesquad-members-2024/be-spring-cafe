a3f415d160d990cf8e0827afef97ee044391637f
hiidy <hiidy115@gmail.com>
2024-04-15 11:51:18 +0900

    feat: UserDatabase에 대한 인터페이스 구현

--- src/main/java/codesquad/springcafe/user/UserDatabase.java
+++ src/main/java/codesquad/springcafe/user/UserDatabase.java
@@ -1,44 +1,17 @@
 package codesquad.springcafe.user;
 
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 
+public interface UserDatabase {
 
-@Repository
-public class UserDatabase {
+    void save(User user);
 
-    private final List<User> userDatabase = new ArrayList<>();
+    void update(User user, String userId);
 
+    User findByUserId(String userId);
 
-    public void save(User user) {
-        userDatabase.add(user);
-    }
+    List<UserRequestDto> findAll();
 
-    public void updateUser(User user, String userId) {
-        userDatabase.remove(findByUserId(userId));
-        save(user);
-    }
-
-    public List<UserRequestDto> findAll() {
-        List<UserRequestDto> userRequestDtos = new ArrayList<>();
-        for (User user : userDatabase) {
-            userRequestDtos.add(
-                new UserRequestDto(user.getUserId(), user.getEmail(), user.getNickname()));
-        }
-        return userRequestDtos;
-    }
-
-    public User findByUserId(String userId) {
-        return userDatabase.stream()
-            .filter(user -> user.getUserId().equals(userId))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("해당하는 userId가 없습니다"));
-    }
-
-    public void clear() {
-        userDatabase.clear();
-    }
-
+    void clear();
 
 }
