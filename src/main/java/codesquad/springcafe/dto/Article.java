8e81a43150e4963747ecb803a48b604d3f83b82f
growth-mango <esudatcom@gmail.com>
2024-04-15 20:44:22 +0900

    feat : Article dto 생성

---
+++ src/main/java/codesquad/springcafe/dto/Article.java
@@ -0,0 +1,33 @@
+package codesquad.springcafe.dto;
+
+public class Article {
+    private Long id; // 자동 생성 아티클 아이디
+    private String writer ;
+    private String title;
+    private String content;
+
+    public Article(String writer, String title, String content) {
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
