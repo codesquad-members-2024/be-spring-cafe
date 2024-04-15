202f3d36fd67cf545549122b31241207ea528ef6
Gyeongtaek <ogt124321@gmail.com>
2024-04-15 13:02:03 +0900

    Add User edit part in UserController
    유저를 수정하는 부분의 맵핑을 추가해주었습니다.
    
    - 기존 profile를 보여주는 부분에서 id로 끝나는 것이 아닌 /profile로 끝나도록 해주었습니다.
    - GetMapping("/{userId}/update") 유저 아이디를 받아와 해당하는 유저가 있는 경우 updateform 으로 유저를 넘겨준다.
    - PutMapping("/{userId}/update") updateform 에서 userId, user, currentPassword를 받아와서 currentPassword가 기존 password와 일치할경우 해당 유저 정보를 수정한다.

--- src/main/java/codesquad/springcafe/controller/UserController.java
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -6,36 +6,80 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/create")
     public String showRegistrationForm(Model model){
         model.addAttribute("user", new User());
+        logger.info("Showing registration form");
         return "/users/form";
     }
+
     @GetMapping("/list")
     public String showUserListForm(Model model){
         model.addAttribute("users",userService.findAll());
+        logger.info("Showing user list");
         return "/users/list";
     }
+
     @PostMapping("/create")
     public String createUser(User user)
     {
         userService.createUser(user);
+        logger.info("User created {}", user);
         return "redirect:/users/list";
     }
-    @GetMapping("/{userId}")
+
+    @GetMapping("/{userId}/profile")
     public String getUserProfile(@PathVariable("userId") String userId, Model model) {
         User user = userService.findByUserId(userId);
         if (user != null) {
+            logger.info("User found {}", user);
             model.addAttribute("user", user);
-            return "users/profile";
+            return "/users/profile";
         }
         return "redirect:/users/list";
+    }
+
+    @GetMapping("/{userId}/update")
+    public String showUserUpdateForm(@PathVariable("userId") String userId, Model model){
+        User user = userService.findByUserId(userId);
+        if (user != null) {
+            logger.info("Update User found {}", user);
+            model.addAttribute("user", user);
+            return "/users/updateform";
+        }
+        return "redirect:/users/list";
+    }
+
+    @PutMapping("/{userId}/update")
+    public String updateUser(@PathVariable("userId") String userId,
+                             @ModelAttribute("user") User user,
+                             @RequestParam("currentPassword") String currentPassword,
+                             Model model) {
+
+        // 현재 비밀번호 확인
+        User currentUser = userService.findByUserId(userId);
+        if (!userService.checkCurrentPassword(currentUser, currentPassword)) {
+            model.addAttribute("error", true);
+            model.addAttribute("user", currentUser);
+            logger.info("CurrentPassword is different");
+            return "/users/updateform";
+        }else{
+            userService.updateUser(userId, user);
+            logger.info("User updated {}", user);
+            return "redirect:/users/list";
+        }
     }
 }
