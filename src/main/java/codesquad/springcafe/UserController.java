d6fe4f78823572c18f2f117d8111e76e3e0f3ca0
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-15 12:01:04 +0900

    feat: 프로필 페이지 조회 기능 추가

--- src/main/java/codesquad/springcafe/UserController.java
+++ src/main/java/codesquad/springcafe/UserController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -25,4 +27,13 @@ public class UserController {
         return "users/list";
     }
 
+    @GetMapping("/users/{userId}")
+    public String userProfile(@PathVariable String userId, Model model){
+        Optional<User> user = UserDatabase.findUserByUserId(userId);
+        if(user.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        model.addAttribute("user", user.get());
+        return "users/profile";
+    }
 }
