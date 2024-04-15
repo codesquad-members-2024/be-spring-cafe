1b186070e9f78875bb902fe607a3842e9b30dd26
Miensoap <miensop51@gmail.com>
2024-04-16 08:00:46 +0900

    세션에서 비밀번호 제거

--- src/main/java/codesquad/springcafe/user/interceptor/LoginCheckInterceptor.java
+++ src/main/java/codesquad/springcafe/user/interceptor/LoginCheckInterceptor.java
@@ -11,12 +11,10 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
-        session.getAttribute("userId");
-        session.getAttribute("password");
-
         SimpleUserInfo loginUser = (SimpleUserInfo) session.getAttribute("loginUser");
         if (loginUser == null) {
             session.setAttribute("toGo", request.getRequestURI());
+
             response.sendRedirect("/user/login");
             return false;
         }
