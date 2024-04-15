4a48eef2e95b4613dd869b7f16d395549f13d235
Gyeongtaek <ogt124321@gmail.com>
2024-04-15 13:19:10 +0900

    Add updateUser and checkCurrentPassword method in UserService class
    매게변수를 통해 기존 유저 정보를 수정할수있는 메서드를 추가해주었습니다.
    
    - updateUser() 받아온 유저 아이디를 통해 유저를 리스트에서 찿고 바뀐유저의 정보로 바꿔서 저장해준다.
    - checkCurrentPassword() 입력한 기존 비밀번호와 유저의 비밀번호가 같은지 확인한다.

--- src/main/java/codesquad/springcafe/service/UserService.java
+++ src/main/java/codesquad/springcafe/service/UserService.java
@@ -10,17 +10,31 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
     public List<User> findAll(){
         return userRepository.findAll();
     }
+
     public User findByUserId(String userId){
         return userRepository.findByUserId(userId);
     }
-    public User createUser(User user){
-        return userRepository.save(user);
+
+    public void createUser(User user){
+        userRepository.save(user);
+    }
+
+    public void updateUser(String userId, User user) {
+        User findUser = findByUserId(userId);
+        findUser.setPassword(user.getPassword());
+        findUser.setName(user.getName());
+        findUser.setEmail(user.getEmail());
+    }
+    public boolean checkCurrentPassword(User user, String currentPassword) {
+        return currentPassword.equals(user.getPassword());
     }
 }
