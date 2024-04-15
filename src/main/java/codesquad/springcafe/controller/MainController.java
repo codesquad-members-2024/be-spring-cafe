08972e36d05301722d44b9f1ab35ab060c74533c
Gyeongtaek <ogt124321@gmail.com>
2024-04-15 10:39:57 +0900

    Add MainController class
    메인 페이지를 동적으로 바꿈에 따른 메인 페이지를 위한 컨트롤러를 추가해주었습니다.
    
    - GetMapping 을 통해 "/" 나 "/index.html"주소가 들어오면 메인 페이지로 보내주었습니다.
    - 메인 페이지를 보여줄때 모든 포스트들을 읽어서 보여줄수있게 해주었습니다.

---
+++ src/main/java/codesquad/springcafe/controller/MainController.java
@@ -0,0 +1,21 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.service.PostService;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    private final PostService postService;
+
+    public MainController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping({"/", "/index.html"})
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("posts", postService.findAll());
+        return "/index";
+    }
+}
