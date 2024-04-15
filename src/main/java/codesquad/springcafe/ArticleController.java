eb8cdd3d85a543895650a193544d923e7d8edd73
imojaek <imojaek@gmail.com>
2024-04-15 12:00:55 +0900

    질문 조회 기능 추가
    article에 Id를 부여하고, 해당 Id를 이용해 "/articles/{articleId}" 에 접근하면 질문의 내용을 확인할 수 있도록 했습니다.

--- src/main/java/codesquad/springcafe/ArticleController.java
+++ src/main/java/codesquad/springcafe/ArticleController.java
@@ -4,7 +4,10 @@ import codesquad.springcafe.database.ArticleDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -17,5 +20,12 @@ public class ArticleController {
         ArticleDatabase.addArticle(article);
         logger.debug("add article : {}", article);
         return "redirect:/";
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public String showArticle(@PathVariable long articleId, Model model) {
+        Article article = ArticleDatabase.getArticle(articleId);
+        model.addAttribute("article", article);
+        return "qna/show";
     }
 }
--- src/main/java/codesquad/springcafe/database/ArticleDatabase.java
+++ src/main/java/codesquad/springcafe/database/ArticleDatabase.java
@@ -3,21 +3,28 @@ package codesquad.springcafe.database;
 import codesquad.springcafe.Article;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ArticleDatabase {
 
-    private final static List<Article> articleList = new ArrayList<>();
+    private final static Map<Long, Article> articleMap = new LinkedHashMap<>();
 
     public static void addArticle(Article article) {
-        articleList.add(article);
+        articleMap.put(article.getArticleId(), article);
     }
 
     public static List<Article> getArticleList() {
-        return articleList;
+        return new ArrayList<>(articleMap.values());
     }
 
     public static List<Article> getReversedArticleList() {
-        return articleList.reversed();
+        return new ArrayList<>(articleMap.values()).reversed();
+    }
+
+    public static Article getArticle(long articleId) {
+        return articleMap.get(articleId);
     }
 }
