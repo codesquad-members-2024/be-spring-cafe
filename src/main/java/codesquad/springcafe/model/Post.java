46bfa2f7df5a08972b7dcdfe6845324cf9fef349
Gyeongtaek <ogt124321@gmail.com>
2024-04-15 10:45:10 +0900

    Add Post class
    게시글에 대한 정보를 담을수 있는 post 객체를 위한 class를 추가해주었습니다.
    
    - Post는 id, titile, author, content, dateTime 변수들을 가지고있습니다.

---
+++ src/main/java/codesquad/springcafe/model/Post.java
@@ -0,0 +1,68 @@
+package codesquad.springcafe.model;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String author;
+    private String content;
+    private LocalDateTime dateTime;
+
+    public Post() {
+
+    }
+
+    public Post(Long id, String title, String author, String content, LocalDateTime dateTime) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Post [id=" + id + ", title=" + title + ", author=" + author + ", content=" + content + ", dateTime=" + dateTime + "]";
+    }
+}
