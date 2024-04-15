65f9be35dbca495f35843ef70b8925d29144bd15
KDT live <dnjsxo0616@gmail.com>
2024-04-15 12:02:23 +0900

    feat: UserRepository 인터페이스를 구현한 MemoryUserRepository 클래스 추가
    - UserRepository 인터페이스를 활용하여 메모리 기반의 사용자 저장소인 MemoryUserRepository 클래스를 추가

---
+++ src/main/java/codesquad/springcafe/web/repository/MemoryUserRepository.java
@@ -0,0 +1,31 @@
+package codesquad.springcafe.web.repository;
+
+import codesquad.springcafe.web.domain.User;
+
+import java.util.*;
+
+public class MemoryUserRepository implements UserRepository {
+    private static List<User> users = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        users.add(user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        for(User user : users) {
+            if(user.getName().equals(name)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+}
