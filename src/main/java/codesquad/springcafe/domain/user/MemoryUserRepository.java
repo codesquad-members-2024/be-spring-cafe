6d6b4f7c05b3983bd8c83cee4b9d99283927cba2
yonghyeonpark <pyh4616@naver.com>
2024-04-15 19:09:08 +0900

    feat: UserRepository 인터페이스 구현
    - 의존성 주입, 다형성을 활용하기 위해 UserRepository 인터페이스 구현

---
+++ src/main/java/codesquad/springcafe/domain/user/MemoryUserRepository.java
@@ -0,0 +1,36 @@
+package codesquad.springcafe.domain.user;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class UserMemoryRepository implements UserRepository {
+
+    private final Map<String, User> users;
+
+    public UserMemoryRepository() {
+        this.users = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void save(User user) {
+        user.setId((long) (users.size() + 1));
+        users.put(user.getUserId(), user);
+    }
+
+    @Override
+    public void update(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    @Override
+    public User findByUserId(String userId) {
+        return users.get(userId);
+    }
+
+    public Map<String, User> getUsers() {
+        return users;
+    }
+}
--- src/main/java/codesquad/springcafe/domain/user/UserRepository.java
+++ src/main/java/codesquad/springcafe/domain/user/UserRepository.java
@@ -1,33 +1,8 @@
 package codesquad.springcafe.domain.user;
 
-import org.springframework.stereotype.Repository;
+public interface UserRepository {
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-@Repository
-public class UserRepository {
-
-    private final Map<String, User> users;
-
-    public UserRepository() {
-        this.users = new ConcurrentHashMap<>();
-    }
-
-    public void save(User user) {
-        user.setId((long) (users.size() + 1));
-        users.put(user.getUserId(), user);
-    }
-
-    public void update(User user) {
-        users.put(user.getUserId(), user);
-    }
-
-    public User findByUserId(String userId) {
-        return users.get(userId);
-    }
-
-    public Map<String, User> getUsers() {
-        return users;
-    }
+    void save(User user);
+    void update(User user);
+    User findByUserId(String userId);
 }
