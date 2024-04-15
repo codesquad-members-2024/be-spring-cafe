07caea87ee4f430e5263e4a10f30418d89a6ed23
seondays <agseon9@gmail.com>
2024-04-15 20:40:45 +0900

    refactor : controller에 연결된 db 변경
    H2 DB와 연결되도록 코드 작성

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,15 +1,23 @@
 package codesquad.springcafe.controller;
 
-import codesquad.springcafe.DB.Database;
+import codesquad.springcafe.DB.H2Database;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final H2Database h2Database;
+
+    @Autowired
+    public HomeController(H2Database h2Database) {
+        this.h2Database = h2Database;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("articles", Database.getAllArticles());
+        model.addAttribute("articles", h2Database.getAllArticles());
         return "index";
     }
 }
