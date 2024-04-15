1ccd652e451e459d31fbd79c15058081590994de
Gyeongtaek <ogt124321@gmail.com>
2024-04-15 11:08:55 +0900

    Add PostController Class
    게시판과 관련된 주소들을 맵핑해주는 클래스를 추가해주었습니다.
    
    - GetMapping("/new") 글작성 form를 보여줍니다.
    - PostMapping("/new") 글을 작성하면 posts 리스트에 저장되고 메인 페이지로 리다이렉트 됩니다.
    - GetMapping("/{postId}") 글의 제목을 클릭하면 게시글의 정보(contents)를 보여줍니다.

---
+++ src/main/java/codesquad/springcafe/controller/PostController.java
@@ -0,0 +1,47 @@
+package codesquad.springcafe.controller;
+
+import codesquad.springcafe.model.Post;
+import codesquad.springcafe.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping("/qna")
+public class PostController {
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/new")
+    public String showPostForm(Model model) {
+        model.addAttribute("post", new Post());
+        logger.info("Showing post form");
+        return "/qna/form";
+    }
+
+    @PostMapping("/new")
+    public String createPost(Post post) {
+        postService.createPost(post);
+        logger.info("Post created {}", post);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{postId}")
+    public String showPostDetails(@PathVariable("postId") long postId, Model model) {
+        Post post = postService.findByPostId(postId);
+        if (post != null) {
+            logger.info("Showing post details for {}", post);
+            model.addAttribute("nlString", System.lineSeparator());
+            model.addAttribute("post", post);
+            return "/qna/show";
+        }
+        return "redirect:/";
+    }
+}
