61c74753c8b73323153288917a92157111acae31
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-16 14:33:48 +0900

    feat: 회원의 세부 정보를 수정할 수 있는 기능 추가

--- src/main/java/codesquad/springcafe/UserController.java
+++ src/main/java/codesquad/springcafe/UserController.java
@@ -4,10 +4,7 @@ import codesquad.springcafe.db.UserDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -44,5 +41,35 @@ public class UserController {
         }
         model.addAttribute("user", user.get());
         return "users/profile";
+    }
+
+    @GetMapping("/users/{userId}/update")
+    public String getProfileEditPage(@PathVariable String userId, Model model){
+        User user = getUserOrFail(userId);
+        model.addAttribute("user", user);
+        return "users/updateForm";
+    }
+
+    @PutMapping("/users/{userId}/update")
+    public String updateProfile(
+            @PathVariable String userId,
+            @RequestParam String email,
+            @RequestParam String password,
+            Model model) {
+
+        User user = getUserOrFail(userId);
+        if(!user.isPasswordInputCorrect(password)){
+            model.addAttribute("user", user);
+            model.addAttribute("passwordError", true);
+            return "users/updateForm";
+        }
+
+        user.setEmail(email);
+        return "redirect:/users";
+    }
+
+    private User getUserOrFail(String userId){
+        return userDatabase.findUserByUserId(userId)
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
