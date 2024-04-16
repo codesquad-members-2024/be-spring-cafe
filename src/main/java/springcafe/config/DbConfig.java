41d2e61961c1f885d0be1c9a220a542ae476ff47
Alex <fnelclsrn123@gmail.com>
2024-04-16 13:16:54 +0900

    feat: jdbc 연결을 위한 설정

---
+++ src/main/java/springcafe/config/DbConfig.java
@@ -0,0 +1,31 @@
+package springcafe.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("org.h2.Driver");
+        driverManagerDataSource.setUrl("jdbc:h2:~/spring-qna-db;DB_CLOSE_ON_EXIT=FALSE");
+        driverManagerDataSource.setUsername("sa");
+        driverManagerDataSource.setPassword("");
+
+
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
