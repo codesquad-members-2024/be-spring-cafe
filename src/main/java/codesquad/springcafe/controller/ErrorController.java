faffaedf125504de98fed7c417c1b49960e6734b
zzawang <hobin0116@naver.com>
2024-04-16 16:45:49 +0900

    chore: 사용하지 않는 매개변수 삭제

--- src/main/java/codesquad/springcafe/controller/ErrorController.java
+++ src/main/java/codesquad/springcafe/controller/ErrorController.java
@@ -3,7 +3,6 @@ package codesquad.springcafe.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +12,7 @@ public class ErrorController {
     private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
     @GetMapping(value = {"/not-found"})
-    public String showErrorPage(Model model) {
+    public String showErrorPage() {
         return "error/not_found";
     }
 }
