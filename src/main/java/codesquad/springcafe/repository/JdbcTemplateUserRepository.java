3cdc457fc976cee1484447ab7ba1b1e64ca4a809
shimbaa <shim9597@gmail.com>
2024-04-17 16:33:52 +0900

    feat: 회원 DB 저장 기능 구현

---
+++ src/main/java/codesquad/springcafe/repository/JdbcTemplateUserRepository.java
@@ -0,0 +1,47 @@
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.model.User;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcTemplateUserRepository implements UserRepository{
+
+    private final NamedParameterJdbcTemplate template;
+
+    public JdbcTemplateUserRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public User save(User user) {
+        String sql = "insert into users (userId, password, name, email) " +
+                "values (:userId, :password, :name, :email)";
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(sql, param, keyHolder);
+
+        Long key = Objects.requireNonNull(keyHolder.getKey()).longValue();
+        user.setId(key);
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        return Optional.empty();
+    }
+}
--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -1,31 +1,14 @@
 package codesquad.springcafe.repository;
 
 import codesquad.springcafe.model.User;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepository {
-    private static final List<User> users = new ArrayList<>();
-    private final Logger logger = LoggerFactory.getLogger(UserRepository.class);
+public interface UserRepository {
 
-    public void add(User user) {
-        users.add(user);
-        logger.info("SAVED USER : {}", user.toString());
-    }
+    User save(User user);
 
-    public List<User> findAll() {
-        return Collections.unmodifiableList(users);
-    }
+    List<User> findAll();
 
-    public Optional<User> findByUserId(String userId) {
-        return users.stream()
-                .filter(user -> user.getUserId().equals(userId))
-                .findAny();
-    }
+    Optional<User> findByUserId(String userId);
 }
