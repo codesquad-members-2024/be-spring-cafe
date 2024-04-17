68cb4a4b91df965fc5524457ceaba5bebad4bcab
Gyeongtaek12345 <ogt124321@gmail.com>
2024-04-17 11:17:36 +0900

    Add methods in PostJdbcRepository which implements PostRepository
    PostJdbcRepository 클라스에서 상속받은 메서드들을 h2 데이터베이스에서 사용할수있게 구현해주었습니다.
    
    - findAll() jdbcTemplate.query를 이용하여 posts database에서 post들을 매핑해서 가져옵니다.
    - save() jdbcTemplate.update를 통해 html에서 받아온 post를 각각의 값에 넣어서 database에 저장합니다.
    - findById() jdbcTemplate.queryForObject를 통해 id가 같은 post를 반환합니다.
    - postRowMapper() RowMapper를 통해 데이터베이스에서 조회한 결과 셋(ResultSet)의 각 행을 post 객체로 매핑합니다.

---
+++ src/main/java/codesquad/springcafe/post/database/PostJdbcRepository.java
@@ -0,0 +1,52 @@
+package codesquad.springcafe.post.database;
+
+import codesquad.springcafe.post.Post;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class PostJdbcRepository implements PostRepository {
+
+    JdbcTemplate jdbcTemplate;
+
+    public PostJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Post> findAll() {
+        String sql = "SELECT * FROM posts";
+        return jdbcTemplate.query(sql,postRowMapper());
+    }
+
+    @Override
+    public void save(Post post) {
+        if (post.getDateTime() == null) {
+            post.setDateTime(LocalDateTime.now());
+        }
+        Timestamp timestamp = Timestamp.valueOf(post.getDateTime());
+        String sql = "INSERT INTO posts (AUTHOR, TITLE, CONTENT, CREATION_TIME) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, post.getAuthor(), post.getTitle(), post.getContent(), timestamp);
+    }
+
+    @Override
+    public Post findById(Long id) {
+        String sql = "SELECT * FROM posts WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, postRowMapper(), id);
+    }
+
+    private RowMapper<Post> postRowMapper() {
+        return (rs, rowNum) -> new Post(
+                rs.getLong("id"),
+                rs.getString("author"),
+                rs.getString("title"),
+                rs.getString("content"),
+                rs.getTimestamp("creation_time").toLocalDateTime()
+        );
+    }
+}
