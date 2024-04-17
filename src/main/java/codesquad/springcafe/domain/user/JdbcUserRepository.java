da874ff4a39c26bfb44477a5cf694f402ccb9ff1
yonghyeonpark <pyh4616@naver.com>
2024-04-17 16:18:44 +0900

    feat: DB 연결을 위한 UserRepository 구현체 생성
    - h2 연결을 위해 JdbcTemplate를 필드로 갖는 JdbcUserRepository 구현

---
+++ src/main/java/codesquad/springcafe/domain/user/JdbcUserRepository.java
@@ -0,0 +1,68 @@
+package codesquad.springcafe.domain.user;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class JdbcUserRepository implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcUserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void save(User user) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("user").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("userId", user.getUserId());
+        parameters.put("password", user.getPassword());
+        parameters.put("name", user.getName());
+        parameters.put("email", user.getEmail());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        user.setId(key.longValue());
+    }
+
+    @Override
+    public void update(User user) {
+        jdbcTemplate.update("update user set password = ?, name = ?, email = ?",
+                user.getPassword(), user.getName(), user.getEmail());
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        return jdbcTemplate.query("select * from user where userId = ?", userRowMapper(), userId)
+                .stream()
+                .findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query("select * from user", userRowMapper());
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> {
+            User user = new User(
+                    rs.getString("userId"),
+                    rs.getString("password"),
+                    rs.getString("name"),
+                    rs.getString("email")
+            );
+            user.setId(rs.getLong("id"));
+            return user;
+        };
+    }
+}
