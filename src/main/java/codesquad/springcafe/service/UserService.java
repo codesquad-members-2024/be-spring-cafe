d8a7e0d7901379bf633bcc917a112db7a0eca856
ohjuyoungg <ejrm12@naver.com>
2024-04-17 23:12:53 +0900

    refactor: userUpdate 메소드명 변경 반영

--- src/main/java/codesquad/springcafe/service/UserService.java
+++ src/main/java/codesquad/springcafe/service/UserService.java
@@ -22,7 +22,7 @@ public class UserService {
 
     private void validateDuplicateUser(User user) {
         // 같은 이름이 있는 중복 회원 X
-        userRepository.findUserById(user.getName())
+        userRepository.findUserById(user.getUserId())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -40,7 +40,6 @@ public class UserService {
     }
 
     public void userUpdate(User updatedUser) {
-        userRepository.updateUser(updatedUser);
+        userRepository.userUpdate(updatedUser);
     }
-
 }
