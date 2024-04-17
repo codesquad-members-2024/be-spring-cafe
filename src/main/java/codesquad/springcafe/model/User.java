8ae280a6aa33f9158992f96cd638761bc5cb618f
shimbaa <shim9597@gmail.com>
2024-04-17 13:20:53 +0900

    refactor: User 클래스 구조 변경
    setter 제거, 매개변수를 가진 생성자 통해 필드값 받도록 설정

--- src/main/java/codesquad/springcafe/model/User.java
+++ src/main/java/codesquad/springcafe/model/User.java
@@ -10,6 +10,13 @@ public class User {
 
     private String email;
 
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
     public String getUserId() {
         return userId;
     }
@@ -24,22 +31,6 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
