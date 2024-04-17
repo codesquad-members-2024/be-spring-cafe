2d971353e323ba6973f9320e663d6b2ab5349fc9
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-17 13:46:24 +0900

    feat: h2 db 기반으로 동작하도록 변경

---
+++ src/main/java/codesquad/springcafe/db/H2UserDatabase.java
@@ -0,0 +1,80 @@
+package codesquad.springcafe.db;
+
+import codesquad.springcafe.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+public class H2UserDatabase implements UserDatabase{
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+
+    public H2UserDatabase(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("users")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    @Override
+    public void addUser(User user) {
+        simpleJdbcInsert.execute(new BeanPropertySqlParameterSource(user));
+    }
+
+    @Override
+    public void update(String userId, User updatedUser) {
+        String sql = "update users set email=? where userid=?";
+        jdbcTemplate.update(
+                sql,
+                updatedUser.getEmail(),
+                updatedUser.getUserId());
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return jdbcTemplate.query("SELECT * FROM users", userRowMapper());
+    }
+
+    @Override
+    public Optional<User> findUserByUserId(String userId) {
+        List<User> result = jdbcTemplate.query("select * from users where userId = ?",
+                userRowMapper(), userId);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public void clearDatabase() {
+        jdbcTemplate.update("delete from users");
+    }
+
+    @Override
+    public int getTotalUserNumber() {
+        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> {
+            String userId = rs.getString("userId");
+            String password = rs.getString("password");
+            String email = rs.getString("email");
+            LocalDateTime registerTime = rs.getTimestamp("registerTime").toLocalDateTime();
+
+            User user = new User();
+            user.setUserId(userId);
+            user.setEmail(email);
+            user.setPassword(password);
+            user.setRegisterTime(registerTime);
+            return user;
+        };
+    }
+}
