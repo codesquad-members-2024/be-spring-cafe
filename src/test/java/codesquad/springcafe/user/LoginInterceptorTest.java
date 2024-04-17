f48a5016e88f2525ad0e33d5ad4b24f726860218
hiidy <hiidy115@gmail.com>
2024-04-17 21:42:04 +0900

    feat: 로그인이 성공하면 이전에 방문하려고 했던 url로 돌아가는 기능 구현

--- src/test/java/codesquad/springcafe/user/LoginInterceptorTest.java
+++ src/test/java/codesquad/springcafe/user/LoginInterceptorTest.java
@@ -35,7 +35,7 @@ class LoginInterceptorTest {
     }
 
     @Test
-    @DisplayName("유저 세션이 null값이 아닐 때 preHandle 테스트")
+    @DisplayName("유저 세션이 존재할 때 preHandle 메서드는 true를 반환해야 함")
     void testPreHandleWithValidSession() throws Exception {
         when(httpServletRequest.getSession()).thenReturn(httpSession);
         when(httpSession.getAttribute("userId")).thenReturn("user");
@@ -47,13 +47,15 @@ class LoginInterceptorTest {
     }
 
     @Test
-    @DisplayName("유저 세션이 null값일 떄 preHandle이 false를 반환하는지 테스트")
+    @DisplayName("유저 세션이 존재하지 않을 때 preHandle 메서드는 false를 반환하고 리다이렉트해야 함")
     void testPreHandleWithInvalidSession() throws Exception {
         when(httpServletRequest.getSession()).thenReturn(httpSession);
+        when(httpServletRequest.getRequestURI()).thenReturn("/");
         when(httpSession.getAttribute("userId")).thenReturn(null);
 
         boolean result = loginInterceptor.preHandle(httpServletRequest, httpServletResponse, null);
         assertThat(result).isFalse();
-        verify(httpServletResponse, times(1)).sendRedirect("/user/login");
+        assertThat(httpServletRequest.getRequestURI()).isEqualTo("/");
+        verify(httpServletResponse, times(1)).sendRedirect("/user/login?return_to=/");
     }
 }
