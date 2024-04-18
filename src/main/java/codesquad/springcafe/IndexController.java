3b6852557fdbb08dd0032fb287a25d8f22c0487b
imojaek <imojaek@gmail.com>
2024-04-18 11:56:26 +0900

    세션 추가
    로그인에 사용할 세션을 추가했습니다.
    
    세션에 userid만 추가해두고, 다른 데이터가 필요한 경우 userid를 통해서 접근할 수 있도록 했습니다.

--- src/main/java/codesquad/springcafe/IndexController.java
+++ src/main/java/codesquad/springcafe/IndexController.java
@@ -2,6 +2,8 @@ package codesquad.springcafe;
 
 import codesquad.springcafe.article.Article;
 import codesquad.springcafe.article.ArticleDatabase;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,9 +22,16 @@ public class IndexController {
     }
 
     @GetMapping({"/", "/index", "/index.html"})
-    public String index(Model model) {
+    public String index(HttpServletRequest request, Model model) {
         List<Article> articleList = articleDatabase.getReversedArticleList();
         model.addAttribute("articleList", articleList);
+
+        HttpSession session = request.getSession(false);
+        model.addAttribute("loginUserId", "X");
+        if (session != null) {
+            String loginUserId = (String) session.getAttribute("userId");
+            model.addAttribute("loginUserId", loginUserId);
+        }
         return "index";
     }
 
--- src/main/java/codesquad/springcafe/user/LoginController.java
+++ src/main/java/codesquad/springcafe/user/LoginController.java
@@ -1,9 +1,11 @@
 package codesquad.springcafe.user;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,12 +20,24 @@ public class LoginController {
     }
 
     @PostMapping("/user/login")
-    public String login(@ModelAttribute UserLoginDTO userLoginDTO, Model model) {
+    public String login(@ModelAttribute UserLoginDTO userLoginDTO, HttpServletRequest request, Model model) {
         if (!userDatabase.isExistUser(userLoginDTO.getUserid()) ||
             !userLoginDTO.isMatchedPwd(userDatabase.getUser(userLoginDTO.getUserid())
                                                     .getPassword())) {
             model.addAttribute("outputMessage", "아이디나 비밀번호가 맞지 않습니다.");
             return "/user/login_failed";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("userid", userLoginDTO.getUserid());
+        return "redirect:/";
+    }
+
+    @PostMapping("/user/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            // 세션 만료
+            session.invalidate();
         }
         return "redirect:/";
     }
