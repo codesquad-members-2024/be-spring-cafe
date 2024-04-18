aa6db92462b54e357d2f20804d392fc6d04de0c9
Yeriimii <seojs0511@naver.com>
2024-04-18 14:30:52 +0900

    chore: sql 스크립트 외래키 제약 조건 추가
    - 외래키 제약 조건 추가
    - repository 폴더의 중복되는 sql 파일 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,17 @@
+-- article 외래키 제약조건 먼저 제거
+alter table if exists article drop constraint if exists fk_created_by;
+
+-- member
+drop table if exists member;
+create table member
+(
+    member_id bigint primary key auto_increment,
+    login_id  varchar(255) unique,
+    password  varchar(255),
+    username  varchar(255),
+    email     varchar(255)
+);
+
 -- article
 drop table if exists article;
 create table article
@@ -6,16 +20,6 @@ create table article
     title      varchar(255),
     contents   longvarchar,
     created_by varchar(255),
-    created_at timestamp
-);
-
--- member
-drop table if exists member;
-create table member
-(
-    member_id bigint primary key auto_increment,
-    login_id  varchar(255),
-    password  varchar(255),
-    username  varchar(255),
-    email     varchar(255)
+    created_at timestamp,
+    constraint fk_created_by foreign key (created_by) references member(login_id) on delete cascade
 );
