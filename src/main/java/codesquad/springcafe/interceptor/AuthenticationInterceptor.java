3c0d992d0fdfc1a91a4fba20cff93c6968dcda59
Keon Kim <fnc6488@gmail.com>
2024-04-18 16:09:35 +0900

    로그인하지 않았으면 로그인 페이지로 이동시키는 기능 구현
    로그인 하지 않은 상태로 사용 가능한 기능: `글 목록 보가`, `사용자 목록 보기`, `회원가입`,`로그인`

---
+++ src/main/java/codesquad/springcafe/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,23 @@
+package codesquad.springcafe.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    public static final String LOGIN_USER_ID = "loginUserId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        HttpSession session = request.getSession();
+        if (session == null || session.getAttribute(LOGIN_USER_ID) == null) {
+            response.sendRedirect("/users/login?redirectURL=" + requestURI);
+            return false;
+        }
+        return true;
+    }
+
+}
