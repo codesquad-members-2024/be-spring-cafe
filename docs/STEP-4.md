804e9daff6369d0d24ac23a693f3eba0942d5cbc
Zoonmy <pjm2571@naver.com>
2024-04-18 11:47:00 +0900

    refactor: User Table, User Model에서 Salt 값 삭제 - User 객체에 존재하는 salt 값 삭제 - User Table에 존재하는 salt column 삭제
    - H2UserRepository sql문 수정
    - MemoryUserRepository 수정
    
    - UserCredentialData의 객체 단위 validate 구현

--- docs/STEP-4.md
+++ docs/STEP-4.md
@@ -56,7 +56,6 @@ CREATE TABLE USERS (
        userId VARCHAR(255) PRIMARY KEY,
        email VARCHAR(255),
        name VARCHAR(255),
-       salt VARCHAR(255),
        hashedPassword VARCHAR(255),
        creationDate VARCHAR(255)
 );
--- src/main/java/codesquad/springcafe/users/service/UserService.java
+++ src/main/java/codesquad/springcafe/users/service/UserService.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -60,8 +62,35 @@ public class UserService {
     }
 
     private void validatePassword(String inputPassword, UserCredentialData userCredentialData) {
-        if (!userCredentialData.verifyPassword(inputPassword)) {
+        UserCredentialData inputCredentialData = new UserCredentialData(hashPassword(inputPassword));
+        if (!inputCredentialData.equals(userCredentialData)) {
             throw new PasswordMismatchException("입력한 비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    private String hashPassword(String password) {
+        try {
+            // SHA-256 알고리즘을 사용하는 MessageDigest 객체 생성
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // 입력 문자열을 바이트 배열로 변환하여 해시 함수에 전달
+            byte[] hash = digest.digest(password.getBytes());
+
+            // 해시된 바이트 배열을 16진수 문자열로 변환
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            // 16진수 해시 문자열 반환
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // 지정된 알고리즘이 없는 경우 예외 처리
+            e.printStackTrace();
+            return null;
         }
     }
 }
