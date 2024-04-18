51776f8e471f6037d75e75e6efc940ae1d762b2f
Daniel <923daniel@naver.com>
2024-04-19 07:00:31 +0900

    refactor: Article 클래스에 생성자 추가
    - h2에서 정보를 가져올 때 사용하는 생성자.

--- src/main/java/codesquad/springcafe/domain/Article.java
+++ src/main/java/codesquad/springcafe/domain/Article.java
@@ -1,21 +1,39 @@
 package codesquad.springcafe.domain;
 
+import jakarta.annotation.Priority;
+import org.springframework.context.annotation.Primary;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Article {
+    private int id;
     private String writer;
     private String title;
     private String content;
     private LocalDateTime time;
-    private String id;
+    private int views;
 
-
+    // ArticleCreateDto를 통해 사용자가 입력한 Article을 저장할 때 사용
     public Article(String writer, String title, String content) {
         this.writer = writer;
         this.title = title;
         this.content = content;
         this.time = LocalDateTime.now();
+    }
+
+    // h2에서 Article 값을 가져올 때 사용하는 생성자
+    public Article(Integer id, String writer, String title, String content, LocalDateTime time, int views) {
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.time = time;
+        this.views = views;
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 
     public String getWriter(){
@@ -34,17 +52,17 @@ public class Article {
         return time;
     }
 
+    public int getViews(){
+        return views;
+    }
+
     public String getFormattedTime(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return time.format(formatter);
     }
 
-    public String getId(){
+    public int getId(){
         return id;
-    }
-
-    public void setId(String id){
-        this.id = id;
     }
 
 
@@ -52,5 +70,5 @@ public class Article {
     public String toString() {
         return "[id:" + id + "] " + "writer: " + writer + ", title: " + title + ", content: " + content;
     }
-    
+
 }
