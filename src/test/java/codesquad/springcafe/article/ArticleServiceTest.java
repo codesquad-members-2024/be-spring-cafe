f528141220741a1b0e4cecaae644f9e05cc499fc
Miensoap <miensop51@gmail.com>
2024-04-19 14:30:08 +0900

    테스트 프로파일 수정
    수정해서 부트 실행 횟수 4 -> 3

--- src/test/java/codesquad/springcafe/article/ArticleServiceTest.java
+++ src/test/java/codesquad/springcafe/article/ArticleServiceTest.java
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ArticleServiceTest {
 
