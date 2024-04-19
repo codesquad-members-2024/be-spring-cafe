a2aaffed56f754f09416175b1d8f8ba5a988eb1a
kalia <fmoon11200@naver.com>
2024-04-19 15:39:48 +0900

    상세보기 html 수정

--- src/main/java/codesquad/springcafe/Member/MemberController.java
+++ src/main/java/codesquad/springcafe/Member/MemberController.java
@@ -1,5 +1,7 @@
 package codesquad.springcafe.Member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,20 +15,22 @@ import java.util.Optional;
 
 @Controller
 public class MemberController {
-    private
-    final MemberRepository memberRepository;
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+    private final MemberRepository memberRepository;
+
     public MemberController(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     @GetMapping("/user/add")
-    public String createform(){
+    public String createForm() {
         return "user/form";
     }
 
     @PostMapping("/user/add")
     public String register(@ModelAttribute Member member) {
         memberRepository.addMember(member);
+        logger.info("새로운 회원 등록: {}", member.getMemberId());
         return "redirect:/user/list";
     }
 
@@ -34,14 +38,19 @@ public class MemberController {
     public String showList(Model model) {
         List<Member> members = memberRepository.getAllMembers();
         model.addAttribute("users", members);
+        logger.info("회원 목록 조회");
         return "user/list";
     }
-    
-    //TODO 아직 안됨 html 수정 필요
+
     @GetMapping("/user/profile/{userId}")
     public String showProfile(Model model, @PathVariable("userId") String memberId) {
         Optional<Member> optionalMember = memberRepository.getMember(memberId);
-        model.addAttribute("user", optionalMember.get());
+        if (optionalMember.isPresent()) {
+            model.addAttribute("user", optionalMember.get());
+            logger.info("회원 프로필 조회: {}", memberId);
+        } else {
+            logger.warn("회원 프로필 조회 실패: {}", memberId);
+        }
         return "user/profile";
     }
 }
--- src/main/java/codesquad/springcafe/Member/MemberRepository.java
+++ src/main/java/codesquad/springcafe/Member/MemberRepository.java
@@ -1,5 +1,7 @@
 package codesquad.springcafe.Member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,17 +10,21 @@ import java.util.Optional;
 
 @Repository
 public class MemberRepository {
+    private static final Logger logger = LoggerFactory.getLogger(MemberRepository.class);
     private final List<Member> members = new ArrayList<>();
 
     public void addMember(Member member) {
         members.add(member);
+        logger.info("회원 추가: {}", member.getMemberId());
     }
 
     public List<Member> getAllMembers() {
+        logger.info("모든 회원 정보 조회");
         return members;
     }
 
     public Optional<Member> getMember(String userId) {
+        logger.info("회원 정보 조회: {}", userId);
         return members.stream()
                 .filter(user -> user.getMemberId().equals(userId))
                 .findFirst();
