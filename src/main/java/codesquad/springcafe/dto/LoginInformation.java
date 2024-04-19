ca5b03625fa80ce7c6f2d4fd1d6ad0912aa8645e
seondays <agseon9@gmail.com>
2024-04-19 20:10:06 +0900

    feat : 로그인 기능 구현
    로그인 요청이 들어오는 경우, 해당 정보를 토대로 유효한 로그인인지 체크하여 세션에 로그인 여부를 세팅한다.

---
+++ src/main/java/codesquad/springcafe/dto/LoginInformation.java
@@ -0,0 +1,19 @@
+package codesquad.springcafe.dto;
+
+public class LoginInformation {
+    private String userId;
+    private String password;
+
+    public LoginInformation(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
