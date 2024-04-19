fbc0604714bac90083d46cf3670dcf32bd0818de
Alex <fnelclsrn123@gmail.com>
2024-04-19 14:29:56 +0900

    feat:로그인 기능, 쿠키로 아이디 기억하기 기능 생성

---
+++ src/main/java/springcafe/user/login/service/LoginService.java
@@ -0,0 +1,33 @@
+package springcafe.user.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import springcafe.user.dto.AuthInfo;
+import springcafe.user.exception.WrongIdPasswordException;
+import springcafe.user.login.security.PasswordHashing;
+import springcafe.user.model.User;
+import springcafe.user.repository.UserDao;
+
+@Service
+public class LoginService {
+
+    private UserDao userDao;
+    private final PasswordHashing passwordHashing;
+
+    @Autowired
+    public LoginService(UserDao userDao, PasswordHashing passwordHashing) {
+        this.userDao = userDao;
+        this.passwordHashing = passwordHashing;
+    }
+
+    public User loginCheck(String id, String password) {
+        User user = userDao.findById(id);
+        String hashedInputPassword = passwordHashing.hashPassword(password);
+
+        if (!user.matchPassword(hashedInputPassword)) {
+            throw new WrongIdPasswordException();
+        }
+
+        return user;
+    }
+}
