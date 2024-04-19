930d95beb73ce222337480f9218a0b03b930bbfa
ryudb0 <ryudb030@gmail.com>
2024-04-19 11:38:24 +0900

    게시글 구현함
    index.html 타임리프 붙여줌

--- src/main/java/codesquad/springcafe/controller/UserController.java
+++ src/main/java/codesquad/springcafe/controller/UserController.java
@@ -2,6 +2,7 @@ package codesquad.springcafe.controller;
 
 import codesquad.springcafe.domain.User;
 import codesquad.springcafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class UserController {
 
     private final UserService service;
 
+    @Autowired
     public UserController(UserService service) {
         this.service = service;
     }
--- src/main/java/codesquad/springcafe/service/UserService.java
+++ src/main/java/codesquad/springcafe/service/UserService.java
@@ -2,6 +2,7 @@ package codesquad.springcafe.service;
 
 import codesquad.springcafe.domain.User;
 import codesquad.springcafe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository repository;
 
+    @Autowired
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
