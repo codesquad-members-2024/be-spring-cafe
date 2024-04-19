f4189f5266098db3f5e7f01e4b9f9af96b46b7bd
sangchu <didi1484@gmail.com>
2024-04-19 10:40:37 +0900

    feat: 게시글 목록에 댓글 수 노출하도록 구현

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,6 +1,11 @@
 package codesquad.springcafe.controller;
 
 import codesquad.springcafe.database.article.ArticleDatabase;
+import codesquad.springcafe.database.comment.CommentDatabase;
+import codesquad.springcafe.model.Article;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     private final ArticleDatabase articleDatabase;
+    private final CommentDatabase commentDatabase;
 
-    public HomeController(ArticleDatabase articleDatabase) {
+    public HomeController(ArticleDatabase articleDatabase, CommentDatabase commentDatabase) {
         this.articleDatabase = articleDatabase;
+        this.commentDatabase = commentDatabase;
     }
 
     /**
@@ -20,7 +27,19 @@ public class HomeController {
      */
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("articles", articleDatabase.findAll());
+
+        List<Article> articles = articleDatabase.findAll();
+        model.addAttribute("articles", articles);
+
+        Map<Long, Long> commentCounts = articles.stream()
+                .collect(Collectors.toMap(
+                        Article::getId, // 'Article' 객체 자체를 키로 사용
+                        article -> (long) commentDatabase.findAll(article.getId()).size()
+                        // 'findAll'로 댓글 리스트를 가져와서 그 크기를 값으로 사용
+                ));
+
+        model.addAttribute("commentCounts", commentCounts);
+
         return "home/index";
     }
 }
