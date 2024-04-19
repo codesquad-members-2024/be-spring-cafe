8e5bfa572fd19c3c6a3f790ea52ce14f004f8115
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-19 20:23:10 +0900

    feat: 로그아웃 버튼 post 요청으로 변경

--- src/main/java/codesquad/springcafe/WebConfig.java
+++ src/main/java/codesquad/springcafe/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LoginCheckInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/images/**", "/css/**", "/*.ico", "/error", "/layout" ,
+                .excludePathPatterns("/", "/images/**", "/css/**", "/*.ico", "/error" ,
                         "/user/**", "/login", "/users/add");
     }
 }
