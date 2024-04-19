1a2f778a971d300abac1bb37c06ccca61352b7bf
Miensoap <miensop51@gmail.com>
2024-04-19 14:30:08 +0900

    테스트 프로파일 수정
    수정해서 부트 실행 횟수 4 -> 3

--- src/test/java/codesquad/springcafe/user/UserControllerTest.java
+++ src/test/java/codesquad/springcafe/user/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
