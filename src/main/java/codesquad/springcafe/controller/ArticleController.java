a6d0a05ce449017c09df7b6d8fff2258c57df790
zzawang <hobin0116@naver.com>
2024-04-19 15:50:02 +0900

    feat: 게시글 삭제 기능 추가
    - 삭제하기는 로그인 사용자와 글쓴이의 사용자 아이디가 같은 경우에만 가능하다.
    - 상황에 따라 "다른 사람의 글을 수정할 수 없다."와 같은 에러 메시지를 출력하는 페이지로 이동하도록 구현한다.

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -92,6 +93,12 @@ public class ArticleController {
         SessionUser sessionUser = (SessionUser) httpSession.getAttribute("sessionUser");
         UpdatedArticle updatedArticle = articleUpdateDto.createUpdatedArticle(sessionUser.getUserId());
         articleService.updateArticle(articleId, updatedArticle);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/delete/{articleId}")
+    public String processDelete(@PathVariable long articleId) {
+        articleService.deleteArticle(articleId);
         return "redirect:/";
     }
 
