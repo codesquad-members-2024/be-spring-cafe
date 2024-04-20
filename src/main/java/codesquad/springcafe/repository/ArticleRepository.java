91054016c7cb0e7d6401ea581d9622d220dc8e1a
ohjuyoungg <ejrm12@naver.com>
2024-04-20 15:48:55 +0900

    feat: UserRepository 인터페이스 생성

--- src/main/java/codesquad/springcafe/repository/ArticleRepository.java
+++ src/main/java/codesquad/springcafe/repository/ArticleRepository.java
@@ -1,2 +1,14 @@
-package codesquad.springcafe.repository;public interface ArticleRepository {
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.model.Article;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleRepository {
+    void addArticle(Article article);
+
+    Optional<Article> findById(Long id);
+
+    List<Article> findAll();
 }
