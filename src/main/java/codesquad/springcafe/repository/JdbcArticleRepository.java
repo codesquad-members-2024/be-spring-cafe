3a24698d4c1dab00362259cfeed75cf72e44ac94
ohjuyoungg <ejrm12@naver.com>
2024-04-20 15:49:35 +0900

    feat: JdbxAtricleRepository 클래스 구현

--- src/main/java/codesquad/springcafe/repository/JdbcArticleRepository.java
+++ src/main/java/codesquad/springcafe/repository/JdbcArticleRepository.java
@@ -1,2 +1,57 @@
-package codesquad.springcafe.repository;public class JdbcArticleRepository {
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.model.Article;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcArticleRepository implements ArticleRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcArticleRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void addArticle(Article article) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("articles").usingColumns("id");
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("writer", article.getWriter());
+        parameters.put("title", article.getTitle());
+        parameters.put("contents", article.getContents());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        article.setId(key.longValue());
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        List<Article> result = jdbcTemplate.query("select * from artice where id = ?", articeRowMapper(), id);
+        return Optional.of((Article) result);
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return jdbcTemplate.query("select * from articles", articeRowMapper());
+    }
+
+    private RowMapper<Article> articeRowMapper() {
+        return ((rs, rowNum) -> {
+            Article article = new Article(
+                    rs.getString("writer"),
+                    rs.getString("title"),
+                    rs.getString("contents")
+            );
+            article.setId(rs.getLong("id"));
+            return article;
+        });
+    }
 }
