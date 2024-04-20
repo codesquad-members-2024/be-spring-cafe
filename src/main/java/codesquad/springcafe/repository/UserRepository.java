0bafa81d8097beaac847516567f8b51da8fb6edc
ohjuyoungg <ejrm12@naver.com>
2024-04-20 15:54:44 +0900

    feat: UserRepository 인터페이스 구현

--- src/main/java/codesquad/springcafe/repository/UserRepository.java
+++ src/main/java/codesquad/springcafe/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package codesquad.springcafe.repository;public class UserRepository {
+package codesquad.springcafe.repository;
+
+import codesquad.springcafe.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    void addUser(User user);
+    Optional<User> findUserById(String userId);
+    List<User> findAll();
+    void userUpdate(User user);
 }
