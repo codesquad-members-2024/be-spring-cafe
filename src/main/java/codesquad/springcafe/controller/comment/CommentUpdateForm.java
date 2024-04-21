f7f1e3242e471a796b5a1176b5d5d9eaa0711836
Yeriimii <seojs0511@naver.com>
2024-04-21 23:27:20 +0900

    feat: CommentUpdateForm 기능 구현
    - 댓글을 수정할 때 내용이 비어있는지 검증할 수 있다

---
+++ src/main/java/codesquad/springcafe/controller/comment/CommentUpdateForm.java
@@ -0,0 +1,43 @@
+package codesquad.springcafe.controller.comment;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class CommentUpdateForm {
+    @NotNull
+    private long id;
+    @NotNull
+    private long articleId;
+    @NotEmpty
+    private String content;
+
+    public CommentUpdateForm() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public CommentUpdateForm setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public long getArticleId() {
+        return articleId;
+    }
+
+    public CommentUpdateForm setArticleId(long articleId) {
+        this.articleId = articleId;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public CommentUpdateForm setContent(String content) {
+        this.content = content;
+        return this;
+    }
+}
---
+++ src/test/java/codesquad/springcafe/controller/comment/CommentUpdateFormTest.java
@@ -0,0 +1,74 @@
+package codesquad.springcafe.controller.comment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import codesquad.springcafe.controller.article.UpdateArticle;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class CommentUpdateFormTest {
+
+    @Autowired
+    private Validator validator;
+
+    @DisplayName("게시물 번호, 작성자, 제목, 본문 내용 모두 값이 있으면 검증에 통과된다")
+    @Test
+    void validate_success() {
+        // given
+        CommentUpdateForm form = new CommentUpdateForm();
+        form.setId(1L);
+        form.setArticleId(1L);
+        form.setContent("test title");
+
+        // when
+        Set<ConstraintViolation<CommentUpdateForm>> violations = validator.validate(form);
+
+        Iterator<ConstraintViolation<CommentUpdateForm>> iterator = violations.iterator();
+
+        List<String> message = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            ConstraintViolation<CommentUpdateForm> next = iterator.next();
+            message.add(next.getMessage());
+        }
+
+        // then
+        assertThat(message).isEmpty();
+    }
+
+    @DisplayName("내용이 비어있으면 검증에 실패하고 에러 메시지가 추가된다")
+    @Test
+    void validate_fail_when_empty_title() {
+        // given
+        /* 제목이 없는 경우 */
+        CommentUpdateForm form = new CommentUpdateForm();
+        form.setId(1L);
+        form.setArticleId(1L);
+
+        // when
+        Set<ConstraintViolation<CommentUpdateForm>> violations = validator.validate(form);
+
+        Iterator<ConstraintViolation<CommentUpdateForm>> iterator = violations.iterator();
+
+        List<String> message = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            ConstraintViolation<CommentUpdateForm> next = iterator.next();
+            message.add(next.getMessage());
+        }
+
+        // then
+        assertThat(message).contains("비어 있을 수 없습니다");
+    }
+}
