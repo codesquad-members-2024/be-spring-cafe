22b65304e3c98063e1bef179d4da2b876cb4f375
Yeriimii <seojs0511@naver.com>
2024-04-21 22:30:16 +0900

    feat: Comment 기능 구현
    - 댓글 작성을 위한 도메인 모델
    - 게시물과 1:N 연관관계

---
+++ src/main/java/codesquad/springcafe/domain/comment/Comment.java
@@ -0,0 +1,78 @@
+package codesquad.springcafe.domain.comment;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Comment {
+    private long id;
+    private long articleId;
+    private String content;
+    private String createdBy;
+    private LocalDateTime createdAt;
+
+    public Comment() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Comment setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public long getArticleId() {
+        return articleId;
+    }
+
+    public Comment setArticleId(long articleId) {
+        this.articleId = articleId;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Comment setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public Comment setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+        return this;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Comment setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Comment comment)) {
+            return false;
+        }
+        return getId() == comment.getId() && getArticleId() == comment.getArticleId() && Objects.equals(getContent(),
+                comment.getContent()) && Objects.equals(getCreatedBy(), comment.getCreatedBy())
+                && Objects.equals(getCreatedAt(), comment.getCreatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getArticleId(), getContent(), getCreatedBy(), getCreatedAt());
+    }
+}
