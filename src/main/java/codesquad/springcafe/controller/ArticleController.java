03c62ad7ba83f42e535479efd792300948a81cc1
Keon Kim <fnc6488@gmail.com>
2024-04-21 14:18:58 +0900

    로그인한 사용자가 게시글의 글쓴이가 되도록 변경
    글쓴이 입력을 위한 input 태그를 제거
    로그인한 사용자 아이디가 게시글의 글쓴이가 되도록 변경

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -3,6 +3,7 @@ package codesquad.springcafe.controller;
 import codesquad.springcafe.dto.ArticleRequestDto;
 import codesquad.springcafe.model.Article;
 import codesquad.springcafe.repository.ArticleRepository;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/question")
 public class ArticleController {
+
+    public static final String LOGIN_USER_ID = "loginUserId";
 
     private final Logger log = LoggerFactory.getLogger(ArticleController.class);
     private final ArticleRepository articleRepository;
@@ -26,8 +29,9 @@ public class ArticleController {
     }
 
     @PostMapping
-    public String writeQuestion(@ModelAttribute ArticleRequestDto articleRequestDto) {
-        Article article = new Article(articleRequestDto);
+    public String writeQuestion(@ModelAttribute ArticleRequestDto articleRequestDto, HttpSession session) {
+        String userId = (String) session.getAttribute(LOGIN_USER_ID);
+        Article article = new Article(userId, articleRequestDto);
         articleRepository.save(article);
         return "redirect:/";
     }
