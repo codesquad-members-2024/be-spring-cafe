70e627eb7da8304d6e1bd7f8c99eb7e61990ed37
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-22 11:48:16 +0900

    feat: ControllerAdvice를 활용한 로그인 상태 전역적 관리

---
+++ src/main/java/codesquad/springcafe/advice/UserAdvice.java
@@ -0,0 +1,23 @@
+package codesquad.springcafe.advice;
+
+import codesquad.springcafe.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class UserAdvice {
+
+    @ModelAttribute
+    public void registerLoginInfo(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        boolean isLoggedIn = session != null && session.getAttribute("springCafeMember") != null;
+        model.addAttribute("loginUser", isLoggedIn);
+        if(isLoggedIn){
+            User user = (User) session.getAttribute("springCafeMember");
+            model.addAttribute("userId", user.getUserId());
+        }
+    }
+}
