1f94776ef3eb481b84c1f66874768419ddec6272
growth-mango <esudatcom@gmail.com>
2024-04-22 18:10:51 +0900

    feat : H2 DB 연결

---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+create table "user""user""user"
+(
+    userId VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (userId)
+);
+
--- src/main/java/codesquad/springcafe/service/UserService.java
+++ src/main/java/codesquad/springcafe/service/UserService.java
@@ -9,5 +9,5 @@ public interface UserService {
     User signup(User user);
     Optional<User> findUserByUserId(String userId); // null 이 있을 수도 있을 경우... null 안정성
     List<User> findAllUsers();
-    User updateUser(String userId, User user);
+    User update(String userId, User user);
 }
