e6d415e90bb0b33f7c5c73b147b04269ed435b54
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-22 11:48:16 +0900

    feat: ControllerAdvice를 활용한 로그인 상태 전역적 관리

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -3,7 +3,6 @@ package codesquad.springcafe.controller;
 import codesquad.springcafe.db.article.ArticleDatabase;
 import codesquad.springcafe.model.Article;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,12 +23,9 @@ public class HomeController {
     @GetMapping(value = {"/", "/index.html"})
     public String showArticleList(Model model, HttpServletRequest request){
         List<Article> articles = articleDatabase.findAllArticles();
-        HttpSession session = request.getSession(false);
-        boolean isLoggedIn = session != null && session.getAttribute("springCafeMember") != null;
 
         model.addAttribute("articles", articles);
         model.addAttribute("totalArticleNumber", articleDatabase.getTotalArticleNumber());
-        model.addAttribute("loginUser", isLoggedIn);
         return "article/list";
     }
 }
