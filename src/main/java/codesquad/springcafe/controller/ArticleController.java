0020d27aead73103dc1a7abc4052a66194ad59e5
shimbaa <shim9597@gmail.com>
2024-04-22 16:20:48 +0900

    refactor: url patter 의 공통부분 분리

--- src/main/java/codesquad/springcafe/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/controller/ArticleController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/qna")
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
@@ -19,18 +21,18 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/qna/form")
+    @GetMapping("/form")
     public String qnaForm() {
         return "/qna/form";
     }
 
-    @PostMapping("/qna/questions")
+    @PostMapping("/questions")
     public String qnaCreate(@ModelAttribute Article article) {
         articleRepository.save(article);
         return "redirect:/";
     }
 
-    @GetMapping("/qna/{articleId}")
+    @GetMapping("/{articleId}")
     public String articleShow(@PathVariable Long articleId, Model model) {
         Optional<Article> optionalArticle = articleRepository.findById(articleId);
         if (optionalArticle.isPresent()) {
