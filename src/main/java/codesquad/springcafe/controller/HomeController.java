9d7f2d90a49344fc6c97d749cd45002bf4e720a2
choejieun <wldms5221@icloud.com>
2024-04-22 21:15:04 +0900

    HomeController 수정 글쓰기 기능 추가

--- src/main/java/codesquad/springcafe/controller/HomeController.java
+++ src/main/java/codesquad/springcafe/controller/HomeController.java
@@ -1,13 +1,29 @@
 package codesquad.springcafe.controller;
 
+import codesquad.springcafe.domain.Article;
+import codesquad.springcafe.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final ArticleService articleService;
+
+    @Autowired
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Article> articles = articleService.findArticle();
+        model.addAttribute("articleList", articles);
+
         return "index";
     }
 
