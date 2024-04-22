ece0cbe02b7b55735d2ecfad650656a8ad5095b3
Yeriimii <seojs0511@naver.com>
2024-04-23 00:40:13 +0900

    feat: CommentForm 검증 추가
    - @NotEmpty 추가로 빈 값 검증

--- src/main/java/codesquad/springcafe/controller/comment/CommentForm.java
+++ src/main/java/codesquad/springcafe/controller/comment/CommentForm.java
@@ -1,6 +1,9 @@
 package codesquad.springcafe.controller.comment;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class CommentForm {
+    @NotEmpty
     private String content;
 
     public CommentForm() {
