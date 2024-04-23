96a649bf387f6957e2ceccd7472a95e37f3bf016
growth-mango <esudatcom@gmail.com>
2024-04-23 19:40:01 +0900

    refactor : 안쓰는 패키지 삭제

--- src/main/java/codesquad/springcafe/repository/JdbcTemplateUserRepository.java
+++ src/main/java/codesquad/springcafe/repository/JdbcTemplateUserRepository.java
@@ -7,10 +7,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.*;
 
 @Repository
