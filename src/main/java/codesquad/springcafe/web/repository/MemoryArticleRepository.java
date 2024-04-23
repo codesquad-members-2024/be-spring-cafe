78251c8f1f49d79a6ff943187159afcb6ba1346e
KDT live <dnjsxo0616@gmail.com>
2024-04-23 13:33:34 +0900

    refactor: 주석처리

--- src/main/java/codesquad/springcafe/web/repository/MemoryArticleRepository.java
+++ src/main/java/codesquad/springcafe/web/repository/MemoryArticleRepository.java
@@ -1,40 +1,40 @@
-package codesquad.springcafe.web.repository;
-
-import codesquad.springcafe.web.domain.Article;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-
-public class MemoryArticleRepository implements ArticleRepository{
-    private static List<Article> articles = new ArrayList<>();
-
-    @Override
-    public Article save(Article article) {
-        articles.add(article);
-        return article;
-    }
-
-    @Override
-    public List<Article> articlesAll() {
-        return articles;
-    }
-
+//package codesquad.springcafe.web.repository;
+//
+//import codesquad.springcafe.web.domain.Article;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//
+//public class MemoryArticleRepository implements ArticleRepository{
+//    private static List<Article> articles = new ArrayList<>();
+//
 //    @Override
-//    public int articleSize() {
-//        return articles.size();
+//    public Article save(Article article) {
+//        articles.add(article);
+//        return article;
 //    }
-
-    @Override
-    public Optional<Article> findByIndex(int number) {
-        int index = number - 1;
-        if (index >= 0 && index < articles.size()) {
-            return Optional.of(articles.get(index));
-        } else {
-            return Optional.empty();
-        }
-
-    }
-}
+//
+//    @Override
+//    public List<Article> articlesAll() {
+//        return articles;
+//    }
+//
+////    @Override
+////    public int articleSize() {
+////        return articles.size();
+////    }
+//
+//    @Override
+//    public Optional<Article> findByIndex(int number) {
+//        int index = number - 1;
+//        if (index >= 0 && index < articles.size()) {
+//            return Optional.of(articles.get(index));
+//        } else {
+//            return Optional.empty();
+//        }
+//
+//    }
+//}
--- src/main/java/codesquad/springcafe/web/repository/MemoryUserRepository.java
+++ src/main/java/codesquad/springcafe/web/repository/MemoryUserRepository.java
@@ -1,33 +1,33 @@
-package codesquad.springcafe.web.repository;
-
-import codesquad.springcafe.web.domain.User;
-import org.springframework.stereotype.Repository;
-
-import java.util.*;
-
-
-public class MemoryUserRepository implements UserRepository {
-    private static Map<String, User> users = new HashMap<>();
-
-    @Override
-    public User save(User user) {
-        users.put(user.getUserId(), user);
-        return user;
-    }
-
-    @Override
-    public Optional<User> findByUserId(String userId) {
-        return Optional.ofNullable(users.get(userId));
-    }
-
-    @Override
-    public List<User> findAll() {
-        return new ArrayList<>(users.values());
-    }
-
-    @Override
-    public void userUpdate(User user) {
-        users.put(user.getUserId(), user);
-    }
-
-}
+//package codesquad.springcafe.web.repository;
+//
+//import codesquad.springcafe.web.domain.User;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.*;
+//
+//
+//public class MemoryUserRepository implements UserRepository {
+//    private static Map<String, User> users = new HashMap<>();
+//
+//    @Override
+//    public User save(User user) {
+//        users.put(user.getUserId(), user);
+//        return user;
+//    }
+//
+//    @Override
+//    public Optional<User> findByUserId(String userId) {
+//        return Optional.ofNullable(users.get(userId));
+//    }
+//
+//    @Override
+//    public List<User> findAll() {
+//        return new ArrayList<>(users.values());
+//    }
+//
+//    @Override
+//    public void userUpdate(User user) {
+//        users.put(user.getUserId(), user);
+//    }
+//
+//}
