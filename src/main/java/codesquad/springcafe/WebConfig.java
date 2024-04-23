4101a9e979da1b25037444d4149b7a9867a3f648
Daniel <923daniel@naver.com>
2024-04-23 16:44:05 +0900

    feat: WebConfig 클래스 구현
    - 로그인 상태를 확인해야 하는 경로 설정.

---
+++ src/main/java/codesquad/springcafe/WebConfig.java
@@ -0,0 +1,20 @@
+package codesquad.springcafe;
+
+import codesquad.springcafe.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/images/**", "/js/**", "/fonts/**", "/main",
+                        "/users/form", "/users/login", "/user/create", "/user/login", "/user/logout");
+    }
+
+
+}
