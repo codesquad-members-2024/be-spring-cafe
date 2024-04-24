38ddedc47c7239e34d34374a7a5d0b0519e94feb
Yeriimii <seojs0511@naver.com>
2024-04-25 03:33:55 +0900

    fix: 리소스가 0개 에러 수정
    - 애러 발생 조건 변경

--- src/main/java/codesquad/springcafe/service/comment/CommentManager.java
+++ src/main/java/codesquad/springcafe/service/comment/CommentManager.java
@@ -42,7 +42,7 @@ public class CommentManager implements CommentService {
         Page<Comment> page = repository.findAllByArticleId(articleId, pageRequest);
 
         /* 요청한 페이지 번호가 전체 페이지 개수보다 많으면 ResourceNotFoundException 예외 */
-        if (pageRequest.getPageNumber() >= page.getTotalPages()) {
+        if (pageRequest.getPageNumber() > page.getTotalPages()) {
             throw new ResourceNotFoundException("전체 페이지 개수를 넘는 페이지를 요청할 수 없습니다.");
         }
 
