ea2a7f3a03cda969b18afe7c75ec959c16e745fb
Yeriimii <seojs0511@naver.com>
2024-04-25 02:30:01 +0900

    feat: CommentService 기능 구현
    - PageRequest 를 인자로 받아 Page 로 반환하는 기능 구현
    - 페이지 요청에 대한 검증 기능 구현

--- src/main/java/codesquad/springcafe/service/comment/CommentService.java
+++ src/main/java/codesquad/springcafe/service/comment/CommentService.java
@@ -2,6 +2,8 @@ package codesquad.springcafe.service.comment;
 
 import codesquad.springcafe.controller.comment.CommentUpdateForm;
 import codesquad.springcafe.domain.comment.Comment;
+import codesquad.springcafe.util.Page;
+import codesquad.springcafe.util.PageRequest;
 import java.util.List;
 
 public interface CommentService {
@@ -10,6 +12,8 @@ public interface CommentService {
     Comment findComment(long commentId);
 
     List<Comment> findAllComment(long articleId);
+
+    Page<Comment> findAllComment(long articleId, PageRequest page);
 
     void editComment(String loginId, CommentUpdateForm updateParam);
 
