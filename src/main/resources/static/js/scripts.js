d03a652a36a3fe9144ce83aca652de174affac4e
hiidy <hiidy115@gmail.com>
2024-04-25 01:07:53 +0900

    feat: ajax를 이용한 비동기적 댓글 작성 기능 구현

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,15 @@
-String.prototype.format = function() {
+String.prototype.format = function () {
   var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
+  return this.replace(/{(\d+)}/g, function (match, number) {
     return typeof args[number] != 'undefined'
         ? args[number]
         : match
         ;
+  });
+};
+
+String.prototype.formatJson = function (json) {
+  return this.replace(/{(\w+)}/g, function (match, key) {
+    return typeof json[key] !== 'undefined' ? json[key] : match;
   });
 };
