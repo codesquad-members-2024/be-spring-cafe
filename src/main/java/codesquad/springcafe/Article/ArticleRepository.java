c66427cd764bbf97a6bb11eaabc3987bf7ba1c5d
kalia <fmoon11200@naver.com>
2024-04-25 01:24:21 +0900

    Article findByindex 관련 try-catch문으로 리팩토링

--- src/main/java/codesquad/springcafe/Article/ArticleRepository.java
+++ src/main/java/codesquad/springcafe/Article/ArticleRepository.java
@@ -1,6 +1,6 @@
 package codesquad.springcafe.Article;
 
-import codesquad.springcafe.Article.Article;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -27,14 +27,16 @@ public class ArticleRepository {
 
     public Optional<Article> findByIndex(int number) {
         int index = number - 1;
-        if (index >= 0 && index < articles.size()) {
+        try {
+            Article article = articles.get(index);
             logger.info("게시글 조회: index = {}", index);
-            return Optional.of(articles.get(index));
-        } else {
+            return Optional.of(article);
+        } catch (IndexOutOfBoundsException e) {
             logger.warn("게시글 조회 실패: 유효하지 않은 index = {}", index);
             return Optional.empty();
         }
     }
+
 
     public int articleSize() {
         return articles.size();
