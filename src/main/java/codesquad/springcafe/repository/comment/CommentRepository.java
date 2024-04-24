8ad2b8d5fb8895b5007761d75b040a79d3d684b8
Yeriimii <seojs0511@naver.com>
2024-04-25 02:23:41 +0900

    feat: CommentRepository 기능 구현
    - PageRequest 를 인자로 받아 Page 로 반환하는 기능 구현

--- src/main/java/codesquad/springcafe/repository/comment/CommentRepository.java
+++ src/main/java/codesquad/springcafe/repository/comment/CommentRepository.java
@@ -2,6 +2,8 @@ package codesquad.springcafe.repository.comment;
 
 import codesquad.springcafe.controller.comment.CommentUpdateForm;
 import codesquad.springcafe.domain.comment.Comment;
+import codesquad.springcafe.util.Page;
+import codesquad.springcafe.util.PageRequest;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +13,8 @@ public interface CommentRepository {
     Optional<Comment> findById(long id);
 
     List<Comment> findAllByArticleId(long articleId);
+
+    Page<Comment> findAllByArticleId(long articleId, PageRequest pageRequest);
 
     void update(CommentUpdateForm updateParam);
 
