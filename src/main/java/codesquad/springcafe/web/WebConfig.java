31b03a48a003695a0ab91bd8cc7089294b871651
yonghyeonpark <pyh4616@naver.com>
2024-04-25 18:19:22 +0900

    feat: 설정 클래스 구현
    - addInterceptors() : 인터셉터를 등록하고, 해당 인터셉터의 실행 순서와
      적용 경로를 설정합니다.
      - LogInterceptor
      - LoginCheckInterceptor

---
+++ src/main/java/codesquad/springcafe/web/WebConfig.java
@@ -0,0 +1,24 @@
+package codesquad.springcafe.web;
+
+import codesquad.springcafe.web.interceptor.LogInterceptor;
+import codesquad.springcafe.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**", "/", "/login", "/logout", "/users/create");
+    }
+}
