1e7b7373de25c55cb1647f12411f9447b1b2cf1d
Gyeongtaek <ogt124321@gmail.com>
2024-04-25 23:42:26 +0900

    Add post update and delete
    controller와 service에 updatePost와 deletePost 관련 메서드를 추가해주었습니다.

--- src/main/java/codesquad/springcafe/post/PostService.java
+++ src/main/java/codesquad/springcafe/post/PostService.java
@@ -1,5 +1,6 @@
 package codesquad.springcafe.post;
 
+import codesquad.springcafe.error.exception.ResourceNotFoundException;
 import codesquad.springcafe.post.database.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,10 +20,20 @@ public class PostService {
     }
 
     public Post findByPostId(long postId){
+        Post post = postRepository.findById(postId);
+        if(post == null)
+            throw new ResourceNotFoundException("게시글을 찾을수 없습니다." + postId);
         return postRepository.findById(postId);
     }
 
-    public void createPost(Post post){
-        postRepository.save(post);
+    public void createPost(String userId, Post post){
+        postRepository.save(userId, post);
+    }
+    public void updatePost(long postId, Post post){
+        postRepository.updatePost(post,postId);
+    }
+
+    public void deletePost(long postId){
+        postRepository.deletePost(postId);
     }
 }
