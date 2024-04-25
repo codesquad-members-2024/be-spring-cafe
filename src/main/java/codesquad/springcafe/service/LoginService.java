2fbe6c762df935596aeed0736a5e0b4137a6ea9d
ohjuyoungg <ejrm12@naver.com>
2024-04-26 03:28:29 +0900

    feat: null 값일 경우 로그인 실패하는 LoginService 클래스 구현

--- src/main/java/codesquad/springcafe/service/LoginService.java
+++ src/main/java/codesquad/springcafe/service/LoginService.java
@@ -1,2 +1,23 @@
-package codesquad.springcafe.service;public class LoginService {
+package codesquad.springcafe.service;
+
+import codesquad.springcafe.model.User;
+import codesquad.springcafe.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    private final UserRepository userRepository;
+
+    public LoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * @return null이면 로그인 실패
+     */
+    public User login(String loginId, String password) {
+        return userRepository.findUserById(loginId)
+                .filter(u -> u.getPassword().equals(password))
+                .orElse(null);
+    }
 }
