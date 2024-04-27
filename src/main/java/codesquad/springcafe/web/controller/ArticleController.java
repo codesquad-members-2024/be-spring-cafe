d34dfa14e02e09ebf9348b00fabc3e26466cc22d
yonghyeonpark <pyh4616@naver.com>
2024-04-27 21:42:14 +0900

    feat: "/articles/{id}/update" url 처리 메서드 구현
    - Get : 게시글 update form을 반환합니다.
    - Put : form을 통해 넘어온 정보와 url의 id를 이용해서 게시글 수정 로직을
      진행합니다.

--- src/main/java/codesquad/springcafe/web/controller/ArticleController.java
+++ src/main/java/codesquad/springcafe/web/controller/ArticleController.java
@@ -3,6 +3,7 @@ package codesquad.springcafe.web.controller;
 import codesquad.springcafe.domain.user.User;
 import codesquad.springcafe.service.ArticleService;
 import codesquad.springcafe.web.dto.ArticleCreateDto;
+import codesquad.springcafe.web.dto.ArticleUpdateDto;
 import codesquad.springcafe.web.validation.ArticleCreateValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -56,5 +57,21 @@ public class ArticleController {
     public String questionList(Model model) {
         model.addAttribute("articles", articleService.getArticles());
         return "index";
+    }
+
+    @GetMapping("/articles/{id}/update")
+    public String updateArticle(@PathVariable Long id, Model model) {
+        model.addAttribute("article", articleService.findById(id));
+        return "qna/updateForm";
+    }
+
+    @PutMapping("/articles/{id}/update")
+    public String updateArticle(
+            @SessionAttribute(name = "loginUser", required = false) User loginUser,
+            @PathVariable Long id,
+            @ModelAttribute ArticleUpdateDto articleUpdateDto) {
+        // 로그인된 사용자가 글의 주인인지 검증하는 로직 추가 예정
+        articleService.updateArticle(id, articleUpdateDto);
+        return "redirect:/";
     }
 }
