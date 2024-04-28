4f8678ccc0be7adc3cd7bd8e4e8f1921d2cfa8f6
yonghyeonpark <pyh4616@naver.com>
2024-04-28 16:00:16 +0900

    feat: Comment 클래스 구현
    - User 클래스의 id와 Article 클래스의 id를 외래키로 가지도록 필드로
      설정했습니다.

---
+++ src/main/java/codesquad/springcafe/domain/comment/Comment.java
@@ -0,0 +1,49 @@
+package codesquad.springcafe.domain.comment;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+
+    private Long id;
+    private Long userId;
+    private Long articleId;
+    private String writer;
+    private String content;
+    private final LocalDateTime currentTime;
+
+    public Comment(Long userId, Long articleId, String writer, String content, LocalDateTime currentTime) {
+        this.userId = userId;
+        this.articleId = articleId;
+        this.writer = writer;
+        this.content = content;
+        this.currentTime = currentTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCurrentTime() {
+        return currentTime;
+    }
+}
