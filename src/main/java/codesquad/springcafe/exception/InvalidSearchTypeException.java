35183c40513787346539f6eb1195ad376fae274e
sangchu <didi1484@gmail.com>
2024-04-30 12:39:01 +0900

    feat: 잘못된 SearchType 요청시 400 페이지 반환 로직 추가

--- src/main/java/codesquad/springcafe/exception/InvalidSearchTypeException.java
+++ src/main/java/codesquad/springcafe/exception/InvalidSearchTypeException.java
@@ -3,6 +3,6 @@ package codesquad.springcafe.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidSearchTypeException extends RuntimeException {
 }
